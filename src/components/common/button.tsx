546edae57408d5f790182754b4110b407d812583
GeonwooShin <rjsdnslawkd@naver.com>
2022-12-20 14:53:57 +0900

    fix : add props to disabled button
    disabled 버튼에 props로 디자인을 추가했습니다.

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -5,11 +5,8 @@ interface buttonProps {
   disabled?: boolean;
   [key: string]: any;
 }
-const DisabledBtn = tw.button`
-w-full transition cursor-not-allowed opacity-50 h-[52px] text-sm rounded-[32px] border border-transparent hover:[#F5535D]-2 bg-[#F5535D] px-0 text-white leading-3 font-normal
-`;
 
-const Btn = tw.button`
+const ButtonTag = tw.button`
 w-full transition hover:cursor-pointer h-[52px] text-sm rounded-[32px] border border-transparent hover:[#F5535D]-2 bg-[#F5535D] px-0 text-white leading-3 font-normal
 `;
 
@@ -19,10 +16,14 @@ export default function Button({
   ...rest
 }: buttonProps) {
   return disabled ? (
-    <DisabledBtn {...rest} disabled>
+    <ButtonTag
+      {...rest}
+      disabled
+      className="hover:cursor-not-allowed opacity-50"
+    >
       {text}
-    </DisabledBtn>
+    </ButtonTag>
   ) : (
-    <Btn {...rest}>{text}</Btn>
+    <ButtonTag {...rest}>{text}</ButtonTag>
   );
 }
