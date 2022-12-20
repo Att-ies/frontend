ec493be702a138f49c4a39d7403aff4a7831e942
jooooonis <xowns3213@naver.com>
2022-12-20 13:54:44 +0900

    feat : login page wireframe

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,53 @@
+import Layout from '@components/common/layout';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+  return (
+    <Layout>
+      <div className="border-gray-900 mx-auto w-full ">
+        <div className="text-center text-2xl font-light mt-12">로고</div>
+        <form className="space-y-4 mt-4" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col mt-36">
+            <input
+              id="id"
+              className="border-gray-900 border-2"
+              placeholder="아이디 입력"
+              type="text"
+              {...register('id', { required: true })}
+            />
+            {errors.id && <span>This field is required</span>}
+          </div>
+          <div className="flex flex-col">
+            <input
+              id="password"
+              className="border-gray-900 border-2"
+              placeholder="비밀번호 입력"
+              type="password"
+              {...register('password', { required: true })}
+            />
+            {errors.password && <span>This field is required</span>}
+          </div>
+          <button className="w-full text-center text-sm font-light rounded-lg bg-gray-900 text-white py-2 hover:bg-gray-800">
+            로그인
+          </button>
+          <button className="w-full text-center text-sm font-light rounded-lg bg-gray-900 text-white py-2 hover:bg-gray-800">
+            회원가입
+          </button>
+          <p className="w-full text-center">아이디 찾기 | 비밀번호 찾기</p>
+          <button className="w-full text-center text-sm font-light rounded-lg bg-yellow-400 text-black py-2 hover:opacity-80">
+            카카오 로그인
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+}
+
+export default Login;
