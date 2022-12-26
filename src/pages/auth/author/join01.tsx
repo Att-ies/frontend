ae9fddc8930086faa9c88f056b10477bf36961de
jooooonis <xowns3213@naver.com>
2022-12-26 12:59:36 +0900

    feat : slider dots

--- src/pages/auth/author/join01.tsx
+++ src/pages/auth/author/join01.tsx
@@ -1,9 +1,12 @@
 import Layout from '@components/common/Layout';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useSwiper } from 'swiper/react';
 import 'swiper/css';
 import { Navigation } from 'swiper';
 
 function Join() {
+  const swiper = useSwiper();
+  console.log(swiper);
   return (
     <Layout>
       <div>
@@ -18,6 +21,7 @@ function Join() {
           <SwiperSlide>
             <div>
               <div className="w-full h-96 bg-orange-400 aspect-square rounded-md"></div>
+              <div className="w-full"></div>
               <h1 className="text-20 font-medium mt-16">
                 버려지는 작품을 판매해보세요.
               </h1>
@@ -25,6 +29,11 @@ function Join() {
                 졸업작품 혹은 과제물로 열심히 작업 후 <br />
                 버려지고 방치되는게 아깝지 않으신가요?
               </p>
+              <div className="space-x-3 flex justify-center mt-6">
+                <div className="w-2 rounded-full aspect-square bg-[#F5535D]"></div>
+                <div className="w-2 rounded-full aspect-square bg-[#D9D9D9]"></div>
+                <div className="w-2 rounded-full aspect-square bg-[#D9D9D9]"></div>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
@@ -35,6 +44,11 @@ function Join() {
                 마케팅, 배송, 판매까지 저희가 모두 다 해드려요. <br />
                 작가님이 작품에만 집중할 수 있도록 도와줘요.
               </p>
+              <div className="space-x-3 flex justify-center mt-6">
+                <div className="w-2 rounded-full aspect-square bg-[#D9D9D9]"></div>
+                <div className="w-2 rounded-full aspect-square bg-[#F5535D]"></div>
+                <div className="w-2 rounded-full aspect-square bg-[#D9D9D9]"></div>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
@@ -47,6 +61,11 @@ function Join() {
                 아티스트와 컬렉터가 대화할 수 있는 <br />
                 현대미술의 장점! 채팅으로 컬렉터분들과 소통해요.
               </p>
+              <div className="space-x-3 flex justify-center mt-6">
+                <div className="w-2 rounded-full aspect-square bg-[#D9D9D9]"></div>
+                <div className="w-2 rounded-full aspect-square bg-[#D9D9D9]"></div>
+                <div className="w-2 rounded-full aspect-square bg-[#F5535D]"></div>
+              </div>
             </div>
           </SwiperSlide>
         </Swiper>
