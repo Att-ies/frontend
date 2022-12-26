1e10c7e94d3b297527fce62fed24ddcafd698a4a
jooooonis <xowns3213@naver.com>
2022-12-26 13:00:58 +0900

    design : padding

--- src/pages/auth/author/join01.tsx
+++ src/pages/auth/author/join01.tsx
@@ -21,14 +21,15 @@ function Join() {
           <SwiperSlide>
             <div>
               <div className="w-full h-96 bg-orange-400 aspect-square rounded-md"></div>
-              <div className="w-full"></div>
-              <h1 className="text-20 font-medium mt-16">
-                버려지는 작품을 판매해보세요.
-              </h1>
-              <p className="text-14 text-[#767676] mt-2">
-                졸업작품 혹은 과제물로 열심히 작업 후 <br />
-                버려지고 방치되는게 아깝지 않으신가요?
-              </p>
+              <div className="pl-6">
+                <h1 className="text-20 font-medium mt-16">
+                  버려지는 작품을 판매해보세요.
+                </h1>
+                <p className="text-14 text-[#767676] mt-2">
+                  졸업작품 혹은 과제물로 열심히 작업 후 <br />
+                  버려지고 방치되는게 아깝지 않으신가요?
+                </p>
+              </div>
               <div className="space-x-3 flex justify-center mt-6">
                 <div className="w-2 rounded-full aspect-square bg-[#F5535D]"></div>
                 <div className="w-2 rounded-full aspect-square bg-[#D9D9D9]"></div>
@@ -39,11 +40,15 @@ function Join() {
           <SwiperSlide>
             <div>
               <div className="w-full h-96 bg-cyan-500 aspect-square rounded-md"></div>
-              <h1 className="text-20 font-medium mt-16">작품 외에 걱정은 NO</h1>
-              <p className="text-14 text-[#767676] mt-2">
-                마케팅, 배송, 판매까지 저희가 모두 다 해드려요. <br />
-                작가님이 작품에만 집중할 수 있도록 도와줘요.
-              </p>
+              <div className="pl-6">
+                <h1 className="text-20 font-medium mt-16">
+                  작품 외에 걱정은 NO
+                </h1>
+                <p className="text-14 text-[#767676] mt-2">
+                  마케팅, 배송, 판매까지 저희가 모두 다 해드려요. <br />
+                  작가님이 작품에만 집중할 수 있도록 도와줘요.
+                </p>
+              </div>
               <div className="space-x-3 flex justify-center mt-6">
                 <div className="w-2 rounded-full aspect-square bg-[#D9D9D9]"></div>
                 <div className="w-2 rounded-full aspect-square bg-[#F5535D]"></div>
@@ -54,13 +59,15 @@ function Join() {
           <SwiperSlide>
             <div>
               <div className="w-full h-96 bg-lime-400 aspect-square rounded-md"></div>
-              <h1 className="text-20 font-medium mt-16">
-                채팅으로 컬렉터와 소통해요
-              </h1>
-              <p className="text-14 text-[#767676] mt-2">
-                아티스트와 컬렉터가 대화할 수 있는 <br />
-                현대미술의 장점! 채팅으로 컬렉터분들과 소통해요.
-              </p>
+              <div className="pl-6">
+                <h1 className="text-20 font-medium mt-16">
+                  채팅으로 컬렉터와 소통해요
+                </h1>
+                <p className="text-14 text-[#767676] mt-2">
+                  아티스트와 컬렉터가 대화할 수 있는 <br />
+                  현대미술의 장점! 채팅으로 컬렉터분들과 소통해요.
+                </p>
+              </div>
               <div className="space-x-3 flex justify-center mt-6">
                 <div className="w-2 rounded-full aspect-square bg-[#D9D9D9]"></div>
                 <div className="w-2 rounded-full aspect-square bg-[#D9D9D9]"></div>
