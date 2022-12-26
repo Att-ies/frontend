727d35ec9f0d679889fa06e5e5e30ac6613bfd6c
gueit214 <gueit214@naver.com>
2022-12-26 10:49:28 +0900

    feat : show border
    경계선 보여주기

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -5,5 +5,7 @@ interface LayoutProps {
 }
 
 export default function Layout({ children }: LayoutProps) {
-  return <div className="font-Pretendard w-full px-6 py-12">{children}</div>;
+  return (
+    <div className="font-Pretendard w-full px-6 py-12 border">{children}</div>
+  );
 }
