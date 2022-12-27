285356cfdf826e7b229094f8f8949d040264dd0e
gueit214 <gueit214@naver.com>
2022-12-27 12:00:39 +0900

    feat : add navigationBar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import Layout from '@components/common/Layout';
+import NavigationBar from '@components/common/NavigationBar';
 
 export default function Home() {
-  return <Layout>Hello</Layout>;
+  return (
+    <Layout>
+      <NavigationBar />
+    </Layout>
+  );
 }
