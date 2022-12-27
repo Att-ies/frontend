6509b56f6a56b4447d53de5fb19ab65a52c25c31
jooooonis <xowns3213@naver.com>
2022-12-27 10:31:48 +0900

    feat : join page user or auth

--- src/pages/auth/join.tsx
+++ src/pages/auth/join.tsx
@@ -1,93 +1,75 @@
 import Layout from '@components/common/Layout';
-import { useForm } from 'react-hook-form';
+import Navigate from '@components/common/Navigate';
+import Image from 'next/image';
+import { useState } from 'react';
+import tw from 'tailwind-styled-components';
+
+interface joinSelectProps {
+  type: string;
+  label?: string;
+  placeholder?: string;
+  [key: string]: any;
+}
+const JoinAsUserTag = tw.div<joinSelectProps>`
+${(p) => (p.$isUser ? 'border-[#F5535D]' : 'border-[#D8D8D8]')}
+w-full pl-3 h-[52px] cursor-pointer font-medium placeholder-[#999999] text-14 rounded-[4px] flex items-center border-[1px]`;
+
+const JoinAsAuthorTag = tw.div<joinSelectProps>`
+${(p) => (p.$isAuthor ? 'border-[#F5535D]' : 'border-[#D8D8D8]')}
+w-full pl-3 h-[52px] cursor-pointer font-medium placeholder-[#999999] text-14 rounded-[4px] flex items-center border-[1px]`;
 
 function Join() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data: any) => console.log(data);
+  const [isUser, setIsUser] = useState(false);
+  const [isAuthor, setIsAuthor] = useState(false);
+
   return (
     <Layout>
-      <div className="border-gray-900 mx-auto w-full ">
-        <div className="text-center text-2xl font-light mt-12">환영합니다</div>
-        <form className="space-y-4 mt-4" onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col mt-8">
-            <label htmlFor="username">사용자 이름</label>
-            <input
-              id="username"
-              className="border-gray-900 border-2"
-              placeholder="이름을 입력해주세요"
-              type="text"
-              {...register('name', { required: true })}
-            />
-            {errors.id && <span>이름을 입력해주세요</span>}
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="address">주소</label>
-            <input
-              id="address"
-              className="border-gray-900 border-2"
-              type="text"
-              {...register('address', { required: true })}
-            />
-            {errors.password && <span>This field is required</span>}
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="email">이메일</label>
-            <input
-              id="email"
-              className="border-gray-900 border-2"
-              type="email"
-              {...register('email', { required: true })}
-            />
-            {errors.password && <span>This field is required</span>}
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="password">비밀번호</label>
-            <input
-              id="password"
-              className="border-gray-900 border-2"
-              type="password"
-              {...register('password', { required: true })}
-            />
-            {errors.password && <span>This field is required</span>}
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="phone">휴대전화 번호 인증</label>
-            <input
-              id="phone"
-              className="border-gray-900 border-2"
-              type="text"
-              {...register('phone', { required: true })}
-            />
-            {errors.password && <span>This field is required</span>}
-          </div>
-          <div className="flex items-center">
-            <input
-              id="confirm"
-              {...register('confirm', { required: true })}
-              type="checkbox"
-            />
-            <div className="text-sm ml-2">
-              <span className="underline underline-offset-2">
-                이용약관, 개인정보
-              </span>
-              수집 및 이용 동의 (필수)
-            </div>
-          </div>
-          <button className="w-full text-center text-sm font-light rounded-lg border-[1px] border-gray-900 text-black py-2 hover:bg-gray-100">
-            회원가입
-          </button>
-          <button className="w-full text-center text-sm font-light rounded-lg bg-yellow-400 text-black py-2 hover:opacity-80">
-            카카오로 3초만에 시작하기
-          </button>
-          <p className="text-center text-sm underline-offset-2 underline">
-            작가로 가입하시나요?
-          </p>
-        </form>
-      </div>
+      <Navigate message="회원 가입" />
+      <section>
+        <div className="text-18 mt-8">
+          <span className="text-[#F5535D] font-bold ">아띠즈</span>에 오신 것을
+          <br />
+          환영합니다.
+        </div>
+        <div className="text-14 text-[#767676] leading-tight mt-3">
+          회원 가입 후 <br />
+          아띠즈의 다양한 서비스를 만나보세요!
+        </div>
+      </section>
+      <section className="mt-8 space-y-[10px]">
+        <JoinAsUserTag
+          $isUser={isUser}
+          onClick={() => {
+            setIsUser(true);
+            setIsAuthor(false);
+          }}
+        >
+          <Image
+            className="mr-3"
+            src="/svg/icons/icon_user.svg"
+            width={20}
+            height={20}
+            alt="user"
+          />
+          회원으로 가입하기
+        </JoinAsUserTag>
+        <JoinAsAuthorTag
+          $isAuthor={isAuthor}
+          onClick={() => {
+            setIsAuthor(true);
+            setIsUser(false);
+          }}
+        >
+          <Image
+            className="mr-3"
+            src="/svg/icons/icon_author.svg"
+            width={20}
+            height={20}
+            alt="user"
+          />
+          작가로 가입하기
+        </JoinAsAuthorTag>
+      </section>
     </Layout>
   );
 }
