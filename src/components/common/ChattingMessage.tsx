d5af05e7bda586d133954b9cc4f7e186f3af4138
gueit214 <gueit214@naver.com>
2022-12-28 15:34:16 +0900

    feat : chatting compopnent

--- src/components/common/ChattingMessage.tsx
+++ src/components/common/ChattingMessage.tsx
@@ -1,11 +1,35 @@
 import tw from 'tailwind-styled-components';
 
 interface chattingProps {
+  time: number | string;
+  text: string;
+  sender: string;
   [key: string]: any;
 }
 
-const ChattingMessageTag = tw.div``;
-
-export default function ChattingMessage({ ...rest }: chattingProps) {
-  <ChattingMessageTag {...rest}>sdds</ChattingMessageTag>;
+export default function ChattingMessage({
+  time,
+  text,
+  sender,
+  ...rest
+}: chattingProps) {
+  return (
+    <div {...rest}>
+      {sender === 'me' ? (
+        <div className="h-[60px]">
+          <span className="text-12 text-[#767676] px-2">{time}</span>
+          <span className="px-7 py-3 bg-[#F5535D] text-[#FFFFFF] text-[12px] rounded-l-2xl rounded-br-2xl ">
+            {text}
+          </span>
+        </div>
+      ) : (
+        <div>
+          <span className="px-7 py-3 bg-[#EDEDED] text-[#767676] text-[12px] rounded-r-2xl rounded-bl-2xl ">
+            {text}
+          </span>
+          <span className="text-12 text-[#767676] px-2">{time}</span>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/pages/chat/[id].tsx
+++ src/pages/chat/[id].tsx
@@ -12,22 +12,21 @@ const DUMP_CHATTINGLIST = [
   {
     id: '1',
     time: '오전 10:30',
-    message: '온주 작가님 안녕하세요',
+    text: '온주 작가님 안녕하세요',
     sender: 'me',
   },
   {
     id: '2',
     time: '오전 10:30',
-    message: '작품 관련 문의사항이 있어 연락드렸습니다.',
+    text: '작품 관련 문의사항이 있어 연락드렸습니다.',
     sender: 'me',
   },
-  { id: '3', time: '오전 10:33', message: '네 안녕하세요!', sender: 'you' },
+  { id: '3', time: '오전 10:33', text: '네 안녕하세요!', sender: 'you' },
 ];
 
 export default function ChatRoom({ params }: ChatRoomProps) {
   const router = useRouter();
   const { id } = router.query;
-
   return (
     <Layout>
       <header className="absolute top-0 inset-x-0 w-full h-[145px] bg-[#F5535D]">
@@ -42,21 +41,26 @@ export default function ChatRoom({ params }: ChatRoomProps) {
           <div className="text-16 px-5 ">온주</div>
           <div className="text-12 flex items-center">응답시간 : 1시간 이내</div>
           <Image
-            src="/svg/icons/icon_back.svg"
+            src="/svg/icons/icon_dots.svg"
             alt="back"
-            width="13"
+            width="3"
             height="0"
             className="absolute right-5"
           />
         </article>
       </header>
       <section className="absolute top-[120px] inset-x-0 w-full bg-white rounded-xl p-5">
-        <article className="flex items-center justify-center text-center text-[#767676] text-14 h-[40px] ">
+        <article className="flex items-center justify-center text-center text-[#767676] text-14 h-[40px] font-bold">
           2022년 12월 23일
         </article>
-        <article>
+        <article className="  ">
           {DUMP_CHATTINGLIST.map((chattingItem) => (
-            <ChattingMessage key={chattingItem.id} />
+            <ChattingMessage
+              key={chattingItem.id}
+              time={chattingItem.time}
+              text={chattingItem.text}
+              sender={chattingItem.sender}
+            />
           ))}
         </article>
       </section>
