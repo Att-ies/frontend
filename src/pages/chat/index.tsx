a2725b907c4f06e4f43e6c4de0299a2770cadcd5
gueit214 <gueit214@naver.com>
2022-12-28 13:44:32 +0900

    feat : chatRoom state

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,20 +1,26 @@
 import Layout from '@components/common/Layout';
 import Tab from '@components/common/Tab';
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 export default function Chat() {
+  const [chatRoom, setChatRoom] = useState([]);
   return (
     <Layout>
       <section className="font-bold text-16">채팅</section>
-      <section className="flex justify-center items-center h-[650px]">
-        <Image
-          src={'/svg/icons/icon_chat_main.svg'}
-          width="150"
-          height="0"
-          alt="chat"
-        />
-      </section>
+      {chatRoom.length ? (
+        <section></section>
+      ) : (
+        <section className="flex justify-center items-center h-[650px]">
+          <Image
+            src={'/svg/icons/icon_chat_main.svg'}
+            width="150"
+            height="0"
+            alt="chat"
+          />
+        </section>
+      )}
+
       <Tab />
     </Layout>
   );
