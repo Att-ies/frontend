5b37d615fd194cb70f4ef567d162b1bca359a1a1
jooooonis <xowns3213@naver.com>
2022-12-28 15:06:24 +0900

    feat : utills fx for token logic localStorage에 token을 get, set, delete하는 함수들입니다.

---
+++ src/utils/localStorage/token/index.ts
@@ -0,0 +1,31 @@
+import { CONFIG } from '@config';
+import {
+  getLocalStorage,
+  removeLocalStorage,
+  setLocalStorage,
+} from '../helper';
+
+const TOKEN_KEY = CONFIG.AUTH_TOKEN_KEY || '@token';
+
+export type Token = {
+  access: string | null;
+  refresh: string | null;
+  isRegister: boolean;
+};
+
+export const getToken = () => {
+  const token = getLocalStorage<Token>(TOKEN_KEY, {
+    access: null,
+    isRegister: false,
+    refresh: null,
+  });
+  return token;
+};
+
+export const setToken = (token: Token) => {
+  setLocalStorage(TOKEN_KEY, token);
+};
+
+export const deleteToken = () => {
+  removeLocalStorage(TOKEN_KEY);
+};
