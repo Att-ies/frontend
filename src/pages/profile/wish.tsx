85cefcb0baf4b53c9d1301bfc22442315be7fbb6
jooooonis <xowns3213@naver.com>
2022-12-29 14:41:28 +0900

    feat : route back

--- src/pages/profile/wish.tsx
+++ src/pages/profile/wish.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Navigate from '@components/common/Navigate';
 import tw from 'tailwind-styled-components';
 import WishCard from '@components/wish/WishCard';
+import { useRouter } from 'next/router';
 
 interface defaultProps {
   [key: string]: any;
@@ -14,9 +15,16 @@ w-full grid grid-cols-2 gap-x-[15px] gap-y-[23px]
 
 export default function Wish() {
   const [wishList, setWishList] = useState([]);
+  const router = useRouter();
   return (
     <Layout>
-      <Navigate message="찜 목록" isRightButton={false} />
+      <Navigate
+        message="찜 목록"
+        isRightButton={false}
+        handleLeftButton={() => {
+          router.back();
+        }}
+      />
       <WishContainer>
         {[1, 2, 3].map((_, i) => (
           <WishCard key={i} />
