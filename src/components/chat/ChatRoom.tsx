66a3786fc03748e08f67049ec25823a711fc6484
gueit214 <gueit214@naver.com>
2022-12-29 13:27:03 +0900

    feat : chatRoom Component

---
+++ src/components/chat/ChatRoom.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+interface chatRoomProps {
+  chatRoom: {
+    id: string;
+    profileImage: string;
+    name: string;
+    time: string;
+    message: string;
+    notifyCnt: number;
+  };
+  [key: string]: any;
+}
+
+export default function Chatroom({ chatRoom }: chatRoomProps) {
+  const handleChattingRoom = () => {
+    router.push(chatRoom.id);
+  };
+  const router = useRouter();
+  return (
+    <section
+      className="h-[64px] flex items-center relative border-b "
+      onClick={handleChattingRoom}
+    >
+      <article>
+        <Image
+          src={chatRoom.profileImage}
+          alt="profile"
+          width="40"
+          height="0"
+          className="mx-1"
+        />
+      </article>
+
+      <article className="flex flex-col">
+        <div className="flex">
+          <p className="text-[#191919] text-14 flex items-center justify-center font-bold px-2">
+            {chatRoom.name}
+          </p>
+          <p className="text-[#767676] text-12 flex items-center justify-center">
+            {chatRoom.time}
+          </p>
+        </div>
+        <p className="text-[#191919] text-12 flex items-center justify-center px-2">
+          {chatRoom.message}
+        </p>
+      </article>
+      <article className="absolute right-0 bg-[#F5535D] w-[20px] h-[20px] flex justify-center items-center text-[#FFF] text-12 rounded-full">
+        {chatRoom.notifyCnt}
+      </article>
+    </section>
+  );
+}
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -2,6 +2,7 @@ import Layout from '@components/common/Layout';
 import Tab from '@components/common/Tab';
 import React, { useState } from 'react';
 import Image from 'next/image';
+import Chatroom from '@components/chat/ChatRoom';
 
 const DUMP_CHATTING_ROOM_LIST = [
   {
@@ -18,7 +19,7 @@ const DUMP_CHATTING_ROOM_LIST = [
     name: '온주',
     time: '오전 10:33',
     message: '작품에 대해 설명 드릴게요.',
-    notifyCnt: 2,
+    notifyCnt: 4,
   },
   {
     id: '3',
@@ -26,39 +27,19 @@ const DUMP_CHATTING_ROOM_LIST = [
     name: '온주',
     time: '오전 10:33',
     message: '작품에 대해 설명 드릴게요.',
-    notifyCnt: 2,
+    notifyCnt: 6,
   },
 ];
 
 export default function Chat() {
   const [chatRoomList, setChatRoomList] = useState(DUMP_CHATTING_ROOM_LIST);
+
   return (
     <Layout>
       <section className="font-bold text-16">채팅</section>
       {chatRoomList.length ? (
         chatRoomList.map((chatRoom) => (
-          <section className="h-[64px] flex items-center relative ">
-            <article>
-              <Image
-                src={chatRoom.profileImage}
-                alt="profile"
-                width="40"
-                height="0"
-                className="mx-1"
-              />
-            </article>
-
-            <article className="flex flex-col">
-              <div className="flex">
-                <p>{chatRoom.name}</p>
-                <p>{chatRoom.time}</p>
-              </div>
-              <p>{chatRoom.message}</p>
-            </article>
-            <article className="absolute right-0 bg-[#F5535D] w-[20px] h-[20px] flex justify-center items-center text-[#FFF] text-12 rounded-full">
-              {chatRoom.notifyCnt}
-            </article>
-          </section>
+          <Chatroom chatRoom={chatRoom} key={chatRoom.id} />
         ))
       ) : (
         <section className="flex justify-center items-center h-[650px]">
