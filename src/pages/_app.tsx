6c164506d6126efbf2b9e916b24ee9d35ff688ff
jooooonis <xowns3213@naver.com>
2022-12-29 11:26:40 +0900

    feat : react-query setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,23 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { Provider } from 'react-redux';
-import store from '../features/store';
+import store from '@features/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+const queryClient = new QueryClient();
 const persistor = persistStore(store);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="mx-auto w-full max-w-[375px]">
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Component {...pageProps} />
-        </PersistGate>
+        <QueryClientProvider client={queryClient}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Component {...pageProps} />
+          </PersistGate>
+        </QueryClientProvider>
       </Provider>
     </div>
   );
