c928c883b8a45bbe90c0a1abb605f84d493b51c2
jooooonis <xowns3213@naver.com>
2022-12-30 11:07:42 +0900

    feat : button props

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,27 +3,24 @@ import tw from 'tailwind-styled-components';
 interface buttonProps {
   text: string;
   disabled?: boolean;
+  onClick?: () => void;
   [key: string]: any;
 }
 
-const ButtonTag = tw.button`
-w-full transition hover:cursor-pointer h-[52px] text-sm rounded-[4px] border border-transparent hover:[#F5535D]-2 bg-[#F5535D] px-0 text-white leading-3 font-normal
-`;
-
-const DisabledButtonTag = tw.button`
-w-full transition hover:cursor-not-allowed opacity-50 h-[52px] text-sm rounded-[4px] border border-transparent hover:[#F5535D]-2 bg-[#F5535D] px-0 text-white leading-3 font-normal
+const ButtonTag = tw.button<buttonProps>`
+${({ disabled }) => disabled && 'opacity-50 cursor-not-allowed'}
+w-full transition h-[52px] text-sm rounded-[4px] border border-transparent hover:[#F5535D]-2 bg-[#F5535D] px-0 text-white leading-3 font-normal
 `;
 
 export default function Button({
   text,
   disabled = false,
+  onClick,
   ...rest
 }: buttonProps) {
-  return disabled ? (
-    <DisabledButtonTag {...rest} disabled>
+  return (
+    <ButtonTag {...rest} disabled={disabled} onClick={onClick}>
       {text}
-    </DisabledButtonTag>
-  ) : (
-    <ButtonTag {...rest}>{text}</ButtonTag>
+    </ButtonTag>
   );
 }
