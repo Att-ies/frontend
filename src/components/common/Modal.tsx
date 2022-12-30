529c1418b349fe6906ac71c5ab30084da34e59c9
gueit214 <gueit214@naver.com>
2022-12-30 11:34:27 +0900

    feat : add Modal Component props

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -3,7 +3,10 @@ import tw from 'tailwind-styled-components';
 interface ModalProps {
   message: string;
   isModal: boolean;
-  onCloseModal: () => {};
+  isMain: boolean;
+  denyMessage: string;
+  onCloseModal: () => void;
+  onAccept: () => void;
 }
 
 const ModalBackground = tw.div`
@@ -20,16 +23,47 @@ bg-[#F5535D] h-[52px] rounded-b-[4px] text-white flex items-center justify-cente
 `;
 
 const ModalTag = tw.div`
+`;
 
+const MainModalBackground = tw.div`
+  absolute inset-0 bg-[#767676] opacity-60 backdrop-blur-3xl
+`;
+const MainModalInner = tw.div`
+absolute z-10 w-[327px] h-[156px] bg-[#fff] inset-0 m-auto rounded-[4px]
+`;
+const MainModalMessage = tw.div`
+h-[104px] text-[#191919] text-14 rounded-t-[4px] flex items-center justify-center font-bold p-5 text-14
+`;
+
+const MainModalDeny = tw.div`
+w-[126px] h-[34px] border border-[#DBDBDB] rounded-[4px] flex items-center justify-center 
+`;
+const MainModalAccept = tw.div`
+w-[126px] h-[34px]  bg-[#F5535D] rounded-[4px] text-[#FFF] flex items-center justify-center 
 `;
 
 export default function Modal({
   message,
   isModal = false,
+  isMain = false,
   onCloseModal,
+  denyMessage,
+  onAccept,
 }: ModalProps) {
   return (
-    isModal && (
+    isModal &&
+    (isMain ? (
+      <ModalTag>
+        <MainModalBackground onClick={onCloseModal}></MainModalBackground>
+        <MainModalInner>
+          <MainModalMessage>{message}</MainModalMessage>
+          <div className="flex w-full justify-evenly ">
+            <MainModalDeny onClick={onCloseModal}>취소</MainModalDeny>
+            <MainModalAccept onClick={onAccept}>{denyMessage}</MainModalAccept>
+          </div>
+        </MainModalInner>
+      </ModalTag>
+    ) : (
       <ModalTag>
         <ModalBackground onClick={onCloseModal}></ModalBackground>
         <ModalInner>
@@ -37,6 +71,6 @@ export default function Modal({
           <ModalAccept onClick={onCloseModal}>확인</ModalAccept>
         </ModalInner>
       </ModalTag>
-    )
+    ))
   );
 }
