67a78576c26421856635e981c99c311c1371119c
jooooonis <xowns3213@naver.com>
2023-01-03 15:28:38 +0900

    chore : export ButtonProps

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,6 +1,6 @@
 import tw from 'tailwind-styled-components';
 
-interface buttonProps {
+export interface ButtonProps {
   kind?: 'filled' | 'outlined';
   text: string;
   disabled?: boolean;
@@ -8,12 +8,12 @@ interface buttonProps {
   [key: string]: any;
 }
 
-const FilledButton = tw.button<buttonProps>`
+const FilledButton = tw.button<ButtonProps>`
 ${({ disabled }) => disabled && 'opacity-50 cursor-not-allowed'}
 w-full transition h-[52px] text-sm rounded-[4px] hover:opacity-90 border border-transparent hover:[#F5535D]-2 bg-[#F5535D] px-0 text-white leading-3 font-normal shadow-xl shadow-gray
 `;
 
-const OutlinedButton = tw.button<buttonProps>`
+const OutlinedButton = tw.button<ButtonProps>`
 ${({ disabled }) => disabled && 'opacity-50 cursor-not-allowed'}
 w-full transition h-[52px] text-sm rounded-[4px] border border-[#F5535D] hover:[#F5535D]-2 bg-white px-0 text-[#F5535D] leading-3 font-normal
 `;
@@ -24,7 +24,7 @@ export default function Button({
   disabled = false,
   onClick,
   ...rest
-}: buttonProps) {
+}: ButtonProps) {
   return kind === 'filled' ? (
     <FilledButton disabled={disabled} onClick={onClick} {...rest}>
       {text}
