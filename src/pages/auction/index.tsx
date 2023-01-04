a622c30fdf14fe85414c4a0d27c3e5b907eab1ac
gueit214 <gueit214@naver.com>
2023-01-04 14:42:28 +0900

    feat : add home, auction, exhibition routing

---
+++ src/pages/auction/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '@components/common/Layout';
+import Tab from '@components/common/Tab';
+import React from 'react';
+
+export default function auction() {
+  return (
+    <Layout>
+      auction
+      <Tab />
+    </Layout>
+  );
+}
