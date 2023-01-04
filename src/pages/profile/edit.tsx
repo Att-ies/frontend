06b9ca1c8d07441631963d863ff2c306c9d9aebc
GeonwooShin <rjsdnslawkd@naver.com>
2023-01-04 14:48:45 +0900

    feat : check api & component

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -2,32 +2,65 @@ import ErrorMessage from '@components/common/ErrorMessage';
 import Input from '@components/common/Input';
 import Layout from '@components/common/Layout';
 import Navigate from '@components/common/Navigate';
-import React, { useRef, useState, useEffect } from 'react';
+import DoubleCheckButton from '@components/common/DoubleCheckButton';
+import authApi from '@apis/auth/authApi';
+import React, { useState, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '@features/hooks';
 import { useForm } from 'react-hook-form';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 interface MessageForm {
-  name: string;
+  nickname: string;
   email: string;
   education: string;
-  recold: string;
-  introduce: string;
+  history: string;
+  description: string;
   instagram: string;
-  bahance: string;
+  behance: string;
+  image: string;
 }
 
-let role = 'artist';
+const role = 'artist';
 
 export default function Edit() {
-  const router = useRouter();
+  const [nickNameValidation, setNickNameValidation] = useState<boolean>(false);
+  const [emailValidation, setEmailValidation] = useState<boolean>(false);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     watch,
+    setError,
+    clearErrors,
   } = useForm<MessageForm>();
-  const [imageFile, setImageFile] = useState(null);
+
+  const handleDoubleCheckNickName = async () => {
+    // 닉네임 중복확인 API
+  };
+
+  const handleDoubleCheckEmail = async () => {
+    const data = await authApi.getCheckEmail(watch('email'));
+    console.log('Email 중복이면 true 아니면 false : ', data.duplicate);
+    if (data.duplicate) {
+      setError('email', {
+        type: 'email duplicate',
+        message: '이미 가입된 이메일 입니다.',
+      });
+      return;
+    } else if (!data.duplicate) {
+      setEmailValidation(true);
+      clearErrors('email');
+    }
+  };
+
+  const router = useRouter();
+  const onLeftButton = () => {
+    router.push('/profile');
+  };
+
+  const [imageFile, setImageFile] = useState<string>('');
 
   const profileImage = watch('image');
   useEffect(() => {
@@ -37,13 +70,14 @@ export default function Edit() {
     }
   }, [profileImage]);
 
-  const onLeftButton = () => {
-    router.push('/profile');
-  };
   const onSubmit = (form: MessageForm) => {
-    console.log(watch('image')[0]);
-    // 채팅 API전송
+    console.log(form);
+    // 프로필 수정 API전송 (PATCH)
   };
+
+  const dispatch = useAppDispatch();
+  const { education, history, description, instagram, behance } =
+    useAppSelector((state) => state.user);
 
   return (
     <Layout>
@@ -72,7 +106,7 @@ export default function Edit() {
         ) : (
           <div className=" flex justify-center items-center w-[99px] h-[99px] rounded-full border-2 border-[#999999] bg-[#FFFFFF] relative">
             <Image
-              src="/svg/icons/icon_avartar.svg"
+              src="/svg/icons/icon_avatar.svg"
               width="60"
               height="0"
               alt="profile"
@@ -96,11 +130,13 @@ export default function Edit() {
         className="hidden"
         {...register('image')}
       />
-      <section className="">
+      <section className="relative">
         <Input
+          type="text"
           label="닉네임"
           placeholder="닉네임을 입력해 주세요."
-          register={register('name', {
+          $error={errors.nickname ? true : false}
+          register={register('nickname', {
             required: true,
             pattern: {
               value: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$/i,
@@ -108,10 +144,23 @@ export default function Edit() {
             },
           })}
         />
-        {errors.name ? <ErrorMessage message={errors.name.message} /> : ''}
+        <DoubleCheckButton
+          $valid={!nickNameValidation}
+          onClick={handleDoubleCheckNickName}
+          text={nickNameValidation ? '사용가능' : '중복확인'}
+        />
+        {errors.nickname ? (
+          <ErrorMessage message={errors.nickname.message} />
+        ) : (
+          ''
+        )}
+      </section>
+      <section className="relative">
         <Input
+          type="text"
           label="이메일"
           placeholder="이메일을 입력해 주세요."
+          $error={errors.email ? true : false}
           register={register('email', {
             required: true,
             pattern: {
@@ -121,14 +170,22 @@ export default function Edit() {
             },
           })}
         />
+        <DoubleCheckButton
+          $valid={!emailValidation}
+          onClick={handleDoubleCheckEmail}
+          text={emailValidation ? '사용가능' : '중복확인'}
+        />
         {errors.email ? <ErrorMessage message={errors.email.message} /> : ''}
       </section>
 
       {role === 'artist' && (
         <section>
           <Input
+            type="text"
             label="학력"
             placeholder="학교와 학위, 전공 등을 입력해 주세요."
+            value={education}
+            $error={errors.education ? true : false}
             register={register('education', {
               required: true,
             })}
@@ -138,35 +195,43 @@ export default function Edit() {
           )}
 
           <Input
+            type="text"
             label="이력"
             placeholder="이력을 작성해 주세요."
-            register={register('recold', {
+            value={history}
+            $error={errors.history ? true : false}
+            register={register('history', {
               required: true,
             })}
           />
-          {errors.recold && <ErrorMessage message={errors.recold.message} />}
+          {errors.history && <ErrorMessage message={errors.history.message} />}
           <Input
+            type="text"
             label="작가소개"
             placeholder="소개를 작성해 주세요."
-            register={register('introduction', {
+            value={description}
+            $error={errors.description ? true : false}
+            register={register('description', {
               required: true,
             })}
           />
-          {errors.introduction && (
-            <ErrorMessage message={errors.introduction.message} />
+          {errors.description && (
+            <ErrorMessage message={errors.description.message} />
           )}
 
           <article className="flex items-center mt-3">
             <label htmlFor="instagram">
               <Image
-                src="/svg/icons/icon_instagram.svg"
+                src="/svg/icons/icon_instagram_gray.svg"
                 width="22"
                 height="10"
                 className="mr-1"
+                alt="instagram"
               />
             </label>
             <input
               placeholder="인스타그램 추가하기"
+              value={instagram}
               {...register('instagram')}
               id="instagram"
               className="h-[30px] placeholder:text-[#999] text-12 indent-1 "
@@ -174,14 +239,16 @@ export default function Edit() {
 
             <label htmlFor="behance">
               <Image
-                src="/svg/icons/icon_behance.svg"
+                src="/svg/icons/icon_behance_gray.svg"
                 width="20"
                 height="0"
                 className="mr-1"
+                alt="behance"
               />
             </label>
             <input
               placeholder="비헨스 추가하기"
+              value={behance}
               {...register('behance')}
               id="behance"
               className="h-[30px] placeholder:text-[#999] text-12 indent-1"
