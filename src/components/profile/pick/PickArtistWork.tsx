5a1744347c04b1df786f504ac96fa4c76cce1072
gueit214 <gueit214@naver.com>
2023-01-04 13:46:50 +0900

    refact : edit design, delete hard coding

--- src/components/profile/pick/PickArtistWork.tsx
+++ src/components/profile/pick/PickArtistWork.tsx
@@ -10,7 +10,7 @@ export default function PickArtistWork({ title, status }: PickArtistWorkProps) {
       <div className="ml-5">
         <p className="text-14 mb-[6px] font-medium">{title} </p>
         <div
-          className={`flex items-center justify-center w-[42px] text-white h-[17px] text-[10px]
+          className={`flex items-center justify-center w-[52px] text-white h-[17px] text-[10px]
           ${status === '입찰중' ? 'bg-[#4B9E77]' : 'bg-[#191919]'}`}
         >
           {status}
