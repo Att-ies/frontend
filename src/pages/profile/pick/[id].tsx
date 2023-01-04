e58ff7bdd4f5c8f5f69c8493795bc0aac7225144
gueit214 <gueit214@naver.com>
2023-01-04 13:46:35 +0900

    refact : add type, edit design

--- src/pages/profile/pick/[id].tsx
+++ src/pages/profile/pick/[id].tsx
@@ -1,5 +1,5 @@
 import Layout from '@components/common/Layout';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigate from '@components/common/Navigate';
 import tw from 'tailwind-styled-components';
 import { useRouter } from 'next/router';
@@ -9,14 +9,20 @@ import Button from '@components/common/Button';
 import { Tab } from '@headlessui/react';
 import PickArtistWork from '@components/profile/pick/PickArtistWork';
 
-const PICKARTIST_DATA = [
+interface PickArtistForm {
+  id: string;
+  title: string;
+  status: string;
+}
+
+const PICKARTIST_DATA: PickArtistForm[] = [
   {
-    id: 1,
+    id: '1',
     title: '퓨처리즘 자연과 공생하는 미래',
     status: '입찰중',
   },
   {
-    id: 2,
+    id: '2',
     title: '퓨처리즘 자연과 공생하는 미래',
     status: '입찰 완료',
   },
@@ -27,7 +33,7 @@ interface defaultProps {
 }
 
 const PickDetailContainer = tw.div<defaultProps>`
-w-full flex items-center cursor-pointer
+w-full flex items-center cursor-pointer mt-5
 `;
 
 const PickDetailProfile = tw.div<defaultProps>`
@@ -35,11 +41,15 @@ w-[60px] mr-[10px] aspect-square flex justify-center items-center rounded-full b
 `;
 
 export default function PickDetail() {
-  const [pickList, setPickList] = useState(PICKARTIST_DATA);
+  const [pickArtistList, setPickArtistList] =
+    useState<PickArtistForm>(PICKARTIST_DATA);
   const router = useRouter();
-  const handleClick = () => {
+  const handleChat = () => {
     // 채팅하러 가기
   };
+  useEffect(() => {
+    // 나의 픽 작가 상세정보 API
+  }, []);
 
   return (
     <Layout>
@@ -61,15 +71,15 @@ export default function PickDetail() {
       <Button
         text="채팅하기"
         kind="outlined"
-        onClick={handleClick}
-        className="mt-8 mb-5"
+        onClick={handleChat}
+        className="mt-8 mb-5 font-bold"
       />
       <Tab.Group>
         <Tab.List>
-          <Tab className="w-1/2 h-[52px] font-medium text-16 ui-selected:border-b-[1.5px] border-[#191919] ui-selected:text-[#191919] ui-not-selected:border-[#EDEDED] ui-not-selected:border-b ui-not-selected:text-[#999999]">
+          <Tab className="w-1/2 h-[52px] font-bold text-16 ui-selected:border-b-[3px] border-[#191919] ui-selected:text-[#191919] ui-not-selected:border-[#EDEDED] ui-not-selected:border-b ui-not-selected:text-[#999999] ">
             작가소개
           </Tab>
-          <Tab className="w-1/2 h-[52px] font-medium text-16 ui-selected:border-b-[1.5px] border-[#191919] ui-selected:text-[#191919] ui-not-selected:border-[#EDEDED] ui-not-selected:border-b ui-not-selected:text-[#999999]">
+          <Tab className="w-1/2 h-[52px] font-bold text-16 ui-selected:border-b-[3px] border-[#191919] ui-selected:text-[#191919] ui-not-selected:border-[#EDEDED] ui-not-selected:border-b ui-not-selected:text-[#999999] ">
             작품
           </Tab>
         </Tab.List>
@@ -77,14 +87,16 @@ export default function PickDetail() {
           <Tab.Panel className="space-y-8">
             <div className="text-14 space-y-3">
               <p className="font-semibold">소개</p>
-              <p className="leading-5">
+              <p className="leading-5 text-12 font-bold">
                 자연과 공생하는 미래를 꿈꾸며, 자연의 모습을 모티브로
                 작업합니다.
               </p>
             </div>
             <div className="text-14 space-y-3">
               <p className="font-semibold">이력</p>
-              <p className="leading-5">2022 조형예술학과 졸업전시</p>
+              <p className="leading-5 text-12 font-bold">
+                2022 조형예술학과 졸업전시
+              </p>
             </div>
             <div className="text-14">
               <p className="font-semibold">SNS</p>
@@ -96,7 +108,9 @@ export default function PickDetail() {
                     height={20}
                     alt="instagram"
                   />
-                  <p className="leading-5 ml-[6px]">ara__22</p>
+                  <p className="leading-5 ml-[6px] text-12 font-bold">
+                    ara__22
+                  </p>
                 </div>
                 <div className="w-1/2 flex items-center">
                   <Image
@@ -105,17 +119,19 @@ export default function PickDetail() {
                     height={20}
                     alt="instagram"
                   />
-                  <p className="leading-5 ml-[6px]">ara__22</p>
+                  <p className="leading-5 ml-[6px] text-12 font-bold">
+                    ara__22
+                  </p>
                 </div>
               </div>
             </div>
           </Tab.Panel>
           <Tab.Panel className="space-y-6">
-            {pickList.map((work) => (
+            {pickArtistList.map((pickArtist: PickArtistForm) => (
               <PickArtistWork
-                key={work.id}
-                title={work.title}
-                status={work.status}
+                key={pickArtist.id}
+                title={pickArtist.title}
+                status={pickArtist.status}
               />
             ))}
           </Tab.Panel>
