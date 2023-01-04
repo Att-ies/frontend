d598154ae5fe719aaf83eec548cc8dd53bc9c86d
gueit214 <gueit214@naver.com>
2023-01-04 11:11:06 +0900

    refact : refactoring notice, add type

--- src/pages/notice.tsx
+++ src/pages/notice.tsx
@@ -5,25 +5,31 @@ import NoticeItem from '@components/notice/NoticeItem';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
-export default function Notice() {
-  const noticeLists = [
-    { text: '노니님의 작품을 관심 등록 하였습니다.', date: '어제', id: 1 },
-    { text: '라미님의 작품을 관심 등록 하였습니다.', date: '어제', id: 2 },
-    {
-      text: '경매에 등록하신 블루밍작품이 유찰되었습니다.',
-      date: '어제',
-      id: 3,
-    },
-    { text: '라미님의 작품을 관심 등록 하였습니다.', date: '그저께', id: 4 },
-  ];
-  const [notice, setNotice] = useState(noticeLists);
+interface NoticeForm {
+  id: string;
+  text: string;
+  date: string;
+}
 
-  const onRemove = (targetId: number) => {
-    const newNoticeList = notice.filter((notice) => {
+const noticeLists: NoticeForm[] = [
+  { text: '노니님의 작품을 관심 등록 하였습니다.', date: '어제', id: '1' },
+  { text: '라미님의 작품을 관심 등록 하였습니다.', date: '어제', id: '2' },
+  {
+    text: '경매에 등록하신 블루밍작품이 유찰되었습니다.',
+    date: '어제',
+    id: '3',
+  },
+  { text: '라미님의 작품을 관심 등록 하였습니다.', date: '그저께', id: '4' },
+];
+
+export default function Notice() {
+  const [notice, setNotice] = useState<NoticeForm[]>(noticeLists);
+
+  const onRemove = (targetId: string): void => {
+    const newNoticeList = notice.filter((notice: NoticeForm) => {
       return notice.id !== targetId;
     });
     setNotice(newNoticeList);
-    console.log(noticeLists);
   };
   const router = useRouter();
   const handleLeftButton = () => {
@@ -36,8 +42,9 @@ export default function Notice() {
         isRightButton={false}
         message="알림"
       />
-      <h1 className="mt-8 mb-6 font-bold">활동알림</h1>
-      {notice.length !== 0 ? (
+      <h1 className="mt-6 mb-6 font-bold">활동알림</h1>
+
+      {notice.length ? (
         <NoticeItem handler={onRemove} noticeList={notice} />
       ) : (
         <div className="w-[130px] h-[92px] mt-[200px] m-auto flex flex-col justify-center items-center">
