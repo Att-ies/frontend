a2fff9b98827c4391dbc6608ef43653dea8b24df
jooooonis <xowns3213@naver.com>
2023-01-06 15:55:54 +0900

    chore : use interface

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,18 +2,18 @@ export interface Member extends User {
   keywords: string[];
 }
 
-export type User = {
+export interface User {
   userId: string;
   password: string;
   email: string;
   nickname: string;
   telephone: string;
-};
+}
 
-export type Artist = {
+export interface Artist {
   education: string;
   history: string;
   description: string;
   instagram: string;
   behance: string;
-};
+}
