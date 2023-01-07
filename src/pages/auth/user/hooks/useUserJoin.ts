b0ffb1cada6e726a6dace105d432d3b48347e076
gueit214 <gueit214@naver.com>
2023-01-07 11:22:47 +0900

    feat : add user join hook

---
+++ src/pages/auth/user/hooks/useUserJoin.ts
@@ -0,0 +1,37 @@
+import useJWTValidate from 'hooks/queries/useJWTValidate';
+import { useMutation } from 'react-query';
+import { useRouter } from 'next/router';
+import authApi from '@apis/auth/authApi';
+import { useState } from 'react';
+
+const useUserJoin = () => {
+  const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const mutation = useMutation(authApi.postUserAuth, {
+    onSuccess: (res) => {
+      router.push('/auth/login');
+    },
+    onError: (error, variable, context) => {
+      const response = error.response;
+      if (response.status === 200) {
+        router.push('/auth/login');
+      } else if (response.status === 409) {
+        switch (response.data.code) {
+          case 'EXIST_USER_ID':
+            setErrorMessage('존재하는 아이디입니다.');
+            break;
+          case 'EXIST_USER_EMAIL':
+            setErrorMessage('존재하는 이메일입니다.');
+            break;
+          case 'EXIST_NICKNAME':
+            setErrorMessage('존재하는 닉네임입니다.');
+            break;
+        }
+      }
+      //에러 페이지 이동
+    },
+  });
+  return { mutation, errorMessage };
+};
+
+export default useUserJoin;
