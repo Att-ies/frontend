715d1ece78313150a233bacf580ba5d0439174af
gueit214 <gueit214@naver.com>
2023-01-10 14:44:35 +0900

    feat : add get profile api

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,21 +1,21 @@
+import authApi from '@apis/auth/authApi';
 import Layout from '@components/common/Layout';
 import Navigate from '@components/common/Navigate';
+import Tab from '@components/common/Tab';
 import Activity from '@components/mypage/Activity';
 import SettingItem from '@components/mypage/SettingItem';
-import Tab from '@components/common/Tab';
 import ArtItem from '@components/profile/ArtItem';
+import arrow from '@public/svg/icons/icon_arrow_black.svg';
 import notification from '@public/svg/icons/icon_notification.svg';
+import plus from '@public/svg/icons/icon_plus_pink.svg';
+import setting from '@public/svg/icons/icon_setting.svg';
 import user from '@public/svg/icons/icon_user.svg';
 import usergray from '@public/svg/icons/icon_user_gray.svg';
-import setting from '@public/svg/icons/icon_setting.svg';
-import arrow from '@public/svg/icons/icon_arrow_black.svg';
-import plus from '@public/svg/icons/icon_plus_pink.svg';
-import tw from 'tailwind-styled-components';
+import { getToken } from '@utils/localStorage/token';
 import Image from 'next/image';
-import { useAppSelector } from '@features/hooks';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { getToken } from '@utils/localStorage/token';
+import tw from 'tailwind-styled-components';
 
 interface defaultProps {
   [key: string]: any;
@@ -94,8 +94,10 @@ const DUMP_ARTLIST: ArtListForm[] = [
 ];
 
 export default function Profile() {
-  const [artList, setArtList] = useState(DUMP_ARTLIST);
-  const [role, setRole] = useState('');
+  const [artList, setArtList] = useState<ArtListForm[]>(DUMP_ARTLIST);
+  const [role, setRole] = useState<string>('');
+  const [nickname, setNickname] = useState<string>('');
+  const [keywords, setKeywords] = useState<string[]>([]);
   const router = useRouter();
   const handleRightButton = () => {
     router.push('/notice');
@@ -115,14 +117,22 @@ export default function Profile() {
     router.push('/profile/edit');
   };
 
-  const { keywords, nickname } = useAppSelector((state) => state.user);
+  const getProfile = async () => {
+    const response = await authApi.getUserProfile();
+    if (response.status === 200) {
+      setNickname(response.data.nickname);
+      // setKeywords(response.data.keywords)
+    }
+  };
 
   useEffect(() => {
     const token = getToken();
     const role = token.role;
-    if (role) {
-      setRole(role);
-      // 작품목록 불러오기 API
+    getProfile();
+    if (role === 'ROLE_USER') {
+      // 취향 목록 불러오기 API
+    } else {
+      // 취향 목록 & 작품 목록 불러오기 API
     }
   }, [artList]);
 
