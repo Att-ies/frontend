1d90a05b2ed4030b9e62f82915844862e195e908
jooooonis <xowns3213@naver.com>
2023-01-11 10:29:09 +0900

    feat : auction create page layout

---
+++ src/pages/auction/create.tsx
@@ -0,0 +1,11 @@
+import Layout from '@components/common/Layout';
+import Navigate from '@components/common/Navigate';
+import React from 'react';
+
+export default function Auction() {
+  return (
+    <Layout>
+      <Navigate right_message="완료" />
+    </Layout>
+  );
+}
