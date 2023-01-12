b33b3c60983f1e9bb2734b6b1d06e9f511a4b574
gueit214 <gueit214@naver.com>
2023-01-12 10:56:21 +0900

    refactor : edit authAPI

--- src/apis/auth/authApi.ts
+++ src/apis/auth/authApi.ts
@@ -2,7 +2,6 @@ import instance from '@apis/_axios/instance';
 import { AxiosInstance } from 'axios';
 
 import { AuthDTOType, DoubleCheckDTOType } from './authApi.type';
-import { getToken } from '@utils/localStorage/token';
 
 export class AuthApi {
   axios: AxiosInstance = instance;
@@ -11,7 +10,7 @@ export class AuthApi {
   }
 
   async postUserAuth(body: AuthDTOType) {
-    await this.axios.post('/members/join', body);
+    await instance.post('/members/join', body);
   }
 
   async getUserProfile() {
@@ -20,15 +19,11 @@ export class AuthApi {
 
   async postArtistAuth(body: AuthDTOType) {
     try {
-      const res = await this.axios({
-        method: 'POST',
-        url: `/artists/join`,
-        data: body,
+      return await this.axios.post('/artists/join', body, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-      return res;
     } catch (error: any) {
       if (error) {
         return error.response;
@@ -38,12 +33,7 @@ export class AuthApi {
 
   async postLogin(body: AuthDTOType) {
     try {
-      const res = await this.axios({
-        method: 'POST',
-        url: `/members/login`,
-        data: body,
-      });
-      return res;
+      return await this.axios.post('/members/login', body);
     } catch (error: any) {
       if (error) {
         return error.response;
@@ -52,11 +42,7 @@ export class AuthApi {
   }
 
   async postAccessToken(body: AuthDTOType): Promise<AuthDTOType> {
-    const { data } = await this.axios({
-      method: 'POST',
-      url: `/members/token`,
-      data: body,
-    });
+    const { data } = await this.axios.post('/members/token', body);
     return data;
   }
 
@@ -91,12 +77,22 @@ export class AuthApi {
     await this.axios.post('/members/logout');
   }
 
+  async patchUserInfo(data: any) {
+    try {
+      const res = await this.axios.patch('/members', data, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      return res;
+    } catch (err) {
+      return err;
+    }
+  }
+
   async deleteUser() {
     try {
-      const res = await this.axios({
-        method: 'DELETE',
-        url: `/members`,
-      });
+      const res = await this.axios.delete('DELETE', '/members');
       return res;
     } catch (error: any) {
       if (error) {
@@ -107,9 +103,7 @@ export class AuthApi {
 
   async getCheckEmail(email) {
     try {
-      const { data } = await this.axios.get(
-        `/members/check-email?email=${email}`,
-      );
+      const { data } = await this.axios(`/members/check-email?email=${email}`);
       return data;
     } catch (error: any) {
       return error.response;
