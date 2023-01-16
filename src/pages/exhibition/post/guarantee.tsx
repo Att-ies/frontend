96e5ec9cc176e1936391828abf662edf02152c93
jooooonis <xowns3213@naver.com>
2023-01-16 10:47:34 +0900

    feat : 전자서명 구현

--- src/pages/exhibition/post/guarantee.tsx
+++ src/pages/exhibition/post/guarantee.tsx
@@ -1,16 +1,25 @@
 import Layout from '@components/common/Layout';
 import Navigate from '@components/common/Navigate';
+import { setSignature } from '@features/signature/signatureSlice';
 import Image from 'next/image';
-import React from 'react';
+import React, { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import SignatureCanvas from 'react-signature-canvas';
 
 export default function Guarantee() {
+  const canvasRef = useRef(null);
+  const dispatch = useDispatch();
+
   return (
     <Layout>
       <Navigate isRightButton={false} message="작품 보증서" />
       <div className="w-full">
         <div className="flex justify-between text-14 mt-6">
           <span className="font-medium">서명을 입력하세요</span>
-          <div className="text-[#767676] flex">
+          <button
+            onClick={() => canvasRef.current.clear()}
+            className="text-[#767676] flex"
+          >
             <Image
               src="/svg/icons/icon_reset.svg"
               alt="reset"
@@ -18,9 +27,22 @@ export default function Guarantee() {
               height={17}
             />
             <span className="ml-1">다시쓰기</span>
-          </div>
+          </button>
         </div>
-        <div className="h-[183px] bg-[#F8F8FA] mt-2"></div>
+        <div className="mt-2">
+          <SignatureCanvas
+            ref={canvasRef}
+            backgroundColor="#F8F8FA"
+            canvasProps={{
+              width: 327,
+              height: 183,
+            }}
+            onEnd={() => {
+              const dataURL = canvasRef.current.getTrimmedCanvas().toDataURL();
+              dispatch(setSignature(dataURL));
+            }}
+          />
+        </div>
         <div className="mt-6">
           <ul className="text-[#767676] ml-3 text-12 list-disc tracking-tight">
             <li>서명 후 아래 이미지로 보증서에 적용됩니다.</li>
