b55c79af8256ac4488bb0e083ebcdb8d98250173
jooooonis <xowns3213@naver.com>
2023-01-18 11:45:39 +0900

    feat : 선택한 키워드 보여주기

--- src/pages/home/post/index.tsx
+++ src/pages/home/post/index.tsx
@@ -153,6 +153,17 @@ export default function Post() {
               />
             </div>
           </button>
+          <div className="flex flex-wrap mt-3">
+            {keywordList?.map((name) => (
+              <div
+                key={name}
+                id={name}
+                className="border-[#F5535D]  text-[#767676] w rounded-full flex justify-center items-center px-3 py-1 border mr-2 mb-2.5 cursor-pointer text-14"
+              >
+                {name}
+              </div>
+            ))}
+          </div>
         </section>
         <Input
           type="number"
