9dbb85582663206c54291fbdc06af0fefde403ae
jooooonis <xowns3213@naver.com>
2023-01-19 15:30:12 +0900

    feat : instance를 사용하지 않음

--- src/apis/artwork/artworkApi.ts
+++ src/apis/artwork/artworkApi.ts
@@ -1,17 +1,24 @@
-import instance from '@apis/_axios/instance';
-import { getToken } from '@utils/localStorage/token';
+import { getToken } from './../../utils/localStorage/token/index';
 import { ArtworkDTOType } from './artworkApi.type';
-
+import axios from 'axios';
 export class ArtworkApi {
-  async postArtwork(body: ArtworkDTOType): Promise<ArtworkDTOType> {
-    const token = getToken();
-    const { data } = await instance.post('/art-works', body, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        Authorization: token.accessToken,
-      },
-    });
-    return data;
+  async postArtwork(body: FormData): Promise<ArtworkDTOType> {
+    try {
+      const token = getToken();
+      const { data } = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/art-works`,
+        body,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: token.accessToken,
+          },
+        },
+      );
+      return data;
+    } catch (err: any) {
+      return err;
+    }
   }
 }
 
