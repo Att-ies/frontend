6c62a18a35b42cc0af15727b4c0d9c0e76d6e4a8
jooooonis <xowns3213@naver.com>
2023-01-20 13:33:31 +0900

    feat : @types 폴더 에서 전역 type 정의

---
+++ src/@types/index.d.ts
@@ -0,0 +1,39 @@
+interface Artwork {
+  image: string[];
+  guaranteeImage: string;
+  title: string;
+  keywords: string[];
+  productionYear: number;
+  material: string;
+  size: string;
+  price: number;
+  status: string;
+  statusDescription: string;
+  frame: boolean;
+  genre: string;
+  description: string;
+  length: number;
+  width: number;
+  height: number;
+}
+
+interface Member extends User, Artist {
+  keywords?: string[];
+}
+
+interface User {
+  userId: string;
+  password: string;
+  email?: string;
+  nickname?: string;
+  telephone?: string;
+  image?: string;
+}
+
+interface Artist {
+  education?: string;
+  history?: string;
+  description?: string;
+  instagram?: string;
+  behance?: string;
+}
