898fb178c7c0898e3036f6b1317b44828a75fac7
Kuesung Park <gueit214@naver.com>
2023-01-21 16:13:25 +0900

    Update Selectkeyword.tsx

--- src/components/common/Selectkeyword.tsx
+++ src/components/common/Selectkeyword.tsx
@@ -4,7 +4,7 @@ import tw from 'tailwind-styled-components'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 
-import Button from '../../components/common/Button'
+import Button from '@components/common/Button'
 
 interface KeywordForm {
   id: string;
