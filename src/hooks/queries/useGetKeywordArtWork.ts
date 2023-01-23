98ec00da4c5a92c86f308d483131fff437420a4d
gueit214 <gueit214@naver.com>
2023-01-23 13:25:55 +0900

    feat : 취향 작품 불러오기 훅

---
+++ src/hooks/queries/useGetKeywordArtWork.ts
@@ -0,0 +1,27 @@
+import instance from '@apis/_axios/instance';
+import { useQuery } from 'react-query';
+import { useState } from 'react';
+
+export default function useGetKeywordArtWork() {
+  const [keywordArtWork, setKeywordArtWork] = useState<Member[]>();
+
+  const query = useQuery(
+    'useKeywordArtWork',
+    async () => {
+      const res = await instance('/members/customized-artworks?page=1&limit=5');
+      console.log(res);
+      return res;
+    },
+    {
+      retry: 0,
+      refetchOnWindowFocus: false,
+      onSuccess: (res: any) => {
+        setKeywordArtWork(res?.data);
+      },
+      onError: (error: any) => {
+        console.log(error);
+      },
+    },
+  );
+  return { ...query, keywordArtWork };
+}
