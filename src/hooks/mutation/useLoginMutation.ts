f9ef0c72b48b012b999938e89fb3cddb6f33e924
jooooonis <xowns3213@naver.com>
2023-01-24 23:23:21 +0900

    feat : useLoginMutation

---
+++ src/hooks/mutation/useLoginMutation.ts
@@ -0,0 +1,10 @@
+import authApi from '@apis/auth/authApi';
+import { Token } from '@utils/localStorage/token';
+import { useMutation } from 'react-query';
+
+const useLoginMutation = () => {
+  return useMutation<Token, Error, LoginForm>('useLoginMuation', (data) =>
+    authApi.postLogin(data),
+  );
+};
+export default useLoginMutation;
