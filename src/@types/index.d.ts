8c5efb8675db8713dc434f674aece95d07f1e38c
jooooonis <xowns3213@naver.com>
2023-01-24 23:43:04 +0900

    fix : error data type 추가

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,22 @@
+interface Member extends User, Artist {
+  keywords?: string[];
+}
+
+interface User {
+  userId: string;
+  password: string;
+  email?: string;
+  nickname?: string;
+  telephone?: string;
+  image?: any;
+}
+interface Artist {
+  education?: string;
+  history?: string;
+  description?: string;
+  instagram?: string;
+  behance?: string;
+}
 interface Artwork {
   image: string[];
   guaranteeImage: string;
@@ -17,34 +36,14 @@ interface Artwork {
   height: number;
 }
 
-interface Member extends User, Artist {
-  keywords?: string[];
-}
-
-interface User {
-  userId: string;
-  password: string;
-  email?: string;
-  nickname?: string;
-  telephone?: string;
-  image?: any;
-}
-
-interface Artist {
-  education?: string;
-  history?: string;
-  description?: string;
-  instagram?: string;
-  behance?: string;
-}
-
 interface LoginForm {
   userId: string;
   password: string;
 }
 
 interface Error {
-  status: number;
   code: string;
   detail: string;
+  error: string;
+  status: number;
 }
