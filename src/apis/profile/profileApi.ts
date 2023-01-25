ca42b0319f634824f447bb51fb1fd714f627a351
gueit214 <gueit214@naver.com>
2023-01-25 14:41:38 +0900

    feat : profileAPI 추가

---
+++ src/apis/profile/profileApi.ts
@@ -0,0 +1,23 @@
+import instance from '@apis/_axios/instance';
+
+interface InquiryForm {
+  date: string;
+  time: string;
+  title: string;
+  content: string;
+  status: string;
+  answer: string;
+  id: number;
+}
+
+export class ProfileApi {
+  async getInquiry(): Promise<InquiryForm> {
+    const { data } = await instance.get('/members/ask');
+    console.log(data);
+    return data;
+  }
+}
+
+const profileApi = new ProfileApi();
+
+export default profileApi;
