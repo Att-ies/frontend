1285d3cf84ec39fff657b341bf4e6ddd7ae72dac
gueit214 <gueit214@naver.com>
2023-01-25 14:41:48 +0900

    refactor : inquiry 훅 추가

--- src/hooks/queries/useGetInquiry.ts
+++ src/hooks/queries/useGetInquiry.ts
@@ -1,6 +1,5 @@
-import instance from '@apis/_axios/instance';
 import { useQuery } from 'react-query';
-import { useState } from 'react';
+import profileApi from '@apis/profile/profileApi';
 
 interface InquiryForm {
   date: string;
@@ -12,25 +11,9 @@ interface InquiryForm {
   id: number;
 }
 
-export default function useGetInquiry() {
-  const [inquiryList, setInquiryList] = useState<InquiryForm[]>();
-
-  const query = useQuery(
-    'useInquiry',
-    async () => {
-      const response = await instance('/members/ask');
-      return response;
-    },
-    {
-      retry: 0,
-      refetchOnWindowFocus: false,
-      onSuccess: (response: any) => {
-        setInquiryList(response?.data);
-      },
-      onError: (error: any) => {
-        console.log(error);
-      },
-    },
+const useGetInquiry = () => {
+  return useQuery<InquiryForm, Error>('useInquiry', () =>
+    profileApi.getInquiry(),
   );
-  return { ...query, inquiryList };
-}
+};
+export default useGetInquiry;
