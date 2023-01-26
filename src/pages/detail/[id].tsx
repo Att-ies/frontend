4ba6b7fc4bf554d74f4c1f3b24e3d901bf2a5e3f
gueit214 <gueit214@naver.com>
2023-01-26 17:34:33 +0900

    feat : 서버 사이드 렌더링

--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -1,38 +1,54 @@
 import Layout from '@components/common/Layout';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'stories/Button';
 import { useRouter } from 'next/router';
 import useGetDetail from '@hooks/queries/useGetDetail';
+import useGetProfile from '@hooks/queries/useGetProfile';
 
-export default function Detail() {
+export function getServerSideProps({ params }) {
+  console.log(params);
+  return {
+    props: {
+      params,
+    },
+  };
+}
+
+export default function Detail({ params }) {
   const router = useRouter();
-  const { id } = router.query;
+  const artWorkId = params?.id;
+  console.log(artWorkId);
+  // const artWorkId = router?.query?.id || '0';
   const handleChat = () => {
     // 채팅방 만들기 API
   };
   const handlePurchase = () => {
     // 응찰 페이지로 이동
   };
-  const { data } = useGetDetail(Number(id));
-  console.log(data);
+
+  const { data: userInfo } = useGetProfile();
+  const { data: detailData } = useGetDetail(Number(artWorkId));
+  const { artWork, artist } = detailData || {};
+
   return (
     <>
       <Layout>
         <section>
           <Image
             alt="detail"
-            src="/svg/example/detail.svg"
+            src={artWork?.images[0] || '/svg/example/detail.svg'}
             width="0"
             height="0"
             className="absolute inset-x-0 top-0 w-full"
+            priority
           />
         </section>
         <section className="absolute inset-x-0 top-[13rem] h-full rounded-[1.6rem] bg-white p-5">
           <article>
             <div className="relative">
-              <div className="text-20 font-bold">콰야 녹아내리는 고드름</div>
-              <div className="mt-1">홍익대학교 예술학과</div>
+              <div className="text-20 font-bold">{artWork?.title}</div>
+              <div className="mt-1">{artist?.artistEducation}</div>
             </div>
             <p className="absolute right-5 top-5">
               <span className="bg-[#F8F8FA] py-0.5 px-1 text-12 font-bold text-brand">
@@ -48,17 +64,19 @@ export default function Detail() {
               <span className="inline-block w-[6rem] text-[#767676]">
                 작가명
               </span>
-              <span className="text-[#191919]">아라 | Ara</span>
+              <span className="text-[#191919]">{artist?.artistName} | Ara</span>
             </p>
             <p className="h-7">
               <span className="inline-block w-[6rem] text-[#767676]">
                 제작연도
               </span>
-              <span className="text-[#191919]">2022</span>
+              <span className="text-[#191919]">{artWork?.productionYear}</span>
             </p>
             <p className="h-7">
               <span className="inline-block w-[6rem] text-[#767676]">장르</span>
-              <span className="text-[#191919]">회화 | Painting</span>
+              <span className="text-[#191919]">
+                {artWork?.genre} | Painting
+              </span>
             </p>
             <p className="h-7">
               <span className="inline-block w-[6rem] text-[#767676]">재료</span>
@@ -66,24 +84,33 @@ export default function Detail() {
             </p>
             <p className="h-7">
               <span className="inline-block w-[6rem] text-[#767676]">액자</span>
-              <span className="text-[#191919]">없음</span>
+              <span className="text-[#191919]">
+                {artWork?.frame ? '있음' : '없음'}
+              </span>
             </p>
             <p className="h-7">
               <span className="inline-block w-[6rem] text-[#767676]">크기</span>
-              <span className="text-[#191919]">41x31.8cm | 6호</span>
+              <span className="text-[#191919]">
+                {artWork?.artWorkSize?.width}x{artWork?.artWorkSize?.length}x
+                {artWork?.artWorkSize?.height} | {artWork?.artWorkSize?.size}호
+              </span>
             </p>
           </article>
-          <article className="border-y py-8 px-2    ">
+          <article className="border-y py-8 px-2">
             <div className="text-18 font-bold">작가 프로필</div>
             <div className="my-2 ml-3 flex h-[4rem] w-[4rem] items-center justify-center rounded-full border border-[#999999]">
               <Image
-                src="/svg/icons/profile/icon_avatar.svg"
+                src={
+                  artist?.artistImage || '/svg/icons/profile/icon_avatar.svg'
+                }
                 width="35"
                 height="0"
                 alt="avatar"
               />
             </div>
-            <div className="ml-1 w-[5rem] text-center">아라</div>
+            <div className="ml-1 w-[5rem] text-center">
+              {artist?.artistName}
+            </div>
           </article>
           <article className="border-b py-5">
             <div className="text-18 font-bold">작품 설명</div>
@@ -91,24 +118,25 @@ export default function Detail() {
               세부 사항 등 궁금한 점은 채팅으로 작가와 소통 할 수 있어요.
             </div>
             <div className="py-5">
-              <p className="text-14">
-                대자연을 자신만의 시각적 언어로 표현한다. 작가는 언어보다 시각적
-                언어로 표현한다. 언어보다 시각적으로 사물을 관찰하고 이해한 바를
-                캔버스로 옮긴다. 먼저 속에 떠오르는 형태, 색 그리고 공간의
-                질서를 만들어간다.
-              </p>
-              <div></div>
+              <p className="text-14">{artWork?.description}</p>
+              <div>
+                {artWork?.keywords?.map((keyword: string, idx: number) => (
+                  <div key={idx}>{keyword}</div>
+                ))}
+              </div>
             </div>
           </article>
 
           <Image
             alt="guarantee"
-            src="/svg/example/guarantee.svg"
+            src={artWork?.guaranteeImage || '/svg/example/guarantee.svg'}
             width="0"
             height="0"
             className="w-full py-5"
+            priority
           />
-          <div className="h-[5rem]"></div>
+
+          {/* <div className="h-[5rem]"></div> */}
         </section>
       </Layout>
       <article className="absolute inset-x-0 top-[46rem] m-auto flex w-[20rem] gap-5">
