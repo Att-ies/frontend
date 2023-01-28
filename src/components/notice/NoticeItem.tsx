338ac6576180d4b926121909b1279ceb4889a75c
gueit214 <gueit214@naver.com>
2023-01-28 15:43:40 +0900

    feat : 알림 페이지 디자인 수정 반영 & 알림 별 아이콘 추가

--- src/components/notice/NoticeItem.tsx
+++ src/components/notice/NoticeItem.tsx
@@ -13,28 +13,45 @@ interface NoticeItemProps {
   refetchNotice: () => void;
 }
 
+const icon = {
+  '채팅 알림': 'chat',
+  '작품 등록 완료': 'post',
+  '작가 등록 완료': 'post',
+  응찰: 'bid',
+  '무제 작품 낙찰 성공': 'bid_success',
+  '1대1 문의 알림': 'inquiry',
+  '경매 등록 알림': 'post_auction',
+  '전시회 등록 알림': 'post_exhibition',
+  '작품 유찰 알림': 'bid_fail',
+};
+
 export default function NoticeItem({ notice, refetchNotice }: NoticeItemProps) {
   const handleRemove = async () => {
     await profileApi.deleteNotice(notice?.id);
-    await refetchNotice();
-    //알림 제거 API
+    refetchNotice();
   };
   return (
-    <li className="text-medium flex flex-col border-b-[1px] last:border-none">
-      <div className="flex justify-between pt-4">
-        <span className="text-15 font-bold">{notice.message}</span>
-        <Image
-          src="/svg/icons/icon_grayClose.svg"
-          alt="close"
-          width={20}
-          height={20}
-          className="ml-2"
-          onClick={handleRemove}
-        />
-      </div>
-      <span className="mt-[6px] mb-4 text-10 text-[#999999]">
-        {notice.createdDate}
-      </span>
+    <li className="text-medium relative flex border-b-[1px] py-3 last:border-none">
+      <Image
+        alt=""
+        src={`/svg/icons/notice/icon_notice_${icon[notice?.title]}.svg`}
+        width="40"
+        height="0"
+        className="ml-2 mr-4"
+      />
+      <section className="flex flex-col leading-5">
+        <p className="text-[12px] font-bold">{notice?.title}</p>
+        <p className="flex justify-between text-[14px]">{notice.message}</p>
+        <p className="text-[10px] text-[#999999]">{notice.createdDate}</p>
+      </section>
+      <Image
+        src="/svg/icons/icon_grayClose.svg"
+        alt="close"
+        width={25}
+        height={0}
+        className="absolute inset-y-0 right-[10px] bottom-0 top-0 m-auto cursor-pointer"
+        onClick={handleRemove}
+      />
     </li>
   );
 }
--- src/hooks/queries/useGetNotice.ts
+++ src/hooks/queries/useGetNotice.ts
@@ -4,7 +4,6 @@ import { useQuery } from 'react-query';
 const useGetNotice = () => {
   return useQuery<any, Error>('useGetNotice', () => profileApi.getNotice(), {
     retry: false,
-    refetchOnWindowFocus: false,
   });
 };
 
