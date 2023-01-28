721b5ccaa81dac28855f3affd49f0fe8eecfc270
gueit214 <gueit214@naver.com>
2023-01-28 09:32:20 +0900

    feat : KeywordBox(공통 컴포넌트) 생성

---
+++ src/components/common/KeywordBox.tsx
@@ -0,0 +1,31 @@
+import tw from 'tailwind-styled-components';
+
+interface KeywordBoxProps {
+  text: string;
+  id?: string;
+  focused?: boolean;
+  [key: string]: any;
+}
+
+const KeywordBoxTag = tw.div<defaultProps>``;
+
+export default function KeywordBox({
+  text,
+  id,
+  focused = false,
+  ...rest
+}: KeywordBoxProps) {
+  return (
+    <KeywordBoxTag
+      id={id}
+      className={`${
+        focused
+          ? 'border-brand text-[#767676]'
+          : 'border-[#CECECE] text-[#767676]'
+      }  mr-2 mt-2 flex cursor-pointer items-center justify-center rounded-[19px] border px-3 py-1 text-[14px] text-[#767676]`}
+      {...rest}
+    >
+      {text}
+    </KeywordBoxTag>
+  );
+}
