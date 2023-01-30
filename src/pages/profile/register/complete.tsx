ce29a2bf11c87e9d32f77b5b7ed1e05ae129f117
jooooonis <xowns3213@naver.com>
2023-01-30 10:32:50 +0900

    feat : 작가등록 완료페이지 백그라운드 이미지 추가

--- src/pages/profile/register/complete.tsx
+++ src/pages/profile/register/complete.tsx
@@ -1,7 +1,8 @@
-import Layout from '@components/common/Layout'
-import Navigate from '@components/common/Navigate'
-import React from 'react'
-import { useRouter } from 'next/router'
+import Layout from '@components/common/Layout';
+import Navigate from '@components/common/Navigate';
+import React from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 export default function Complete() {
   const router = useRouter();
@@ -15,8 +16,14 @@ export default function Complete() {
         isLeftButton={false}
         handleRightButton={handleRightButton}
       />
-      <section className="h-[40rem] flex flex-col items-center justify-center text-center ">
-        <div className="text-18 font-bold h-8">작가 등록이 완료되었습니다.</div>
+      <section className="relative flex h-[40rem] flex-col items-center justify-center text-center">
+        <Image
+          alt="congratulations"
+          src="/image/congratulations.png"
+          fill
+          className="object-contain"
+        />
+        <div className="h-8 text-18 font-bold">작가 등록이 완료되었습니다.</div>
         <div className="#767676 text-12 text-[#767676]">
           <p>아띠즈에서 학력 인증 확인 후 알림을 드리겠습니다.</p>
           <p>심사는 3일(영업일) 이내 진행됩니다.</p>
