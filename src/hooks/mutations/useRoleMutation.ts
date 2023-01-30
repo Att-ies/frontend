5062b294edd88180c58f2c5d3169214e7b20b981
jooooonis <xowns3213@naver.com>
2023-01-30 10:19:23 +0900

    feat : mutation hook 작성

---
+++ src/hooks/mutations/useRoleMutation.ts
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router';
+import { getToken, setToken } from '@utils/localStorage/token/index';
+import profileApi from '@apis/profile/profileApi';
+import { Role } from '@apis/profile/profileApi.type';
+import { useMutation } from 'react-query';
+
+const useRoleMutation = () => {
+  const router = useRouter();
+  return useMutation<Role, Error>(
+    'useRoleMuation',
+    () => profileApi.patchRole(),
+    {
+      onSuccess: (data) => {
+        const token = getToken();
+        setToken({
+          ...token,
+          roles: data.roles,
+        });
+        router.push('/profile/edit');
+      },
+    },
+  );
+};
+export default useRoleMutation;
