8524733c4234cfe6066a2fc5e931802b7afee2a8
jooooonis <xowns3213@naver.com>
2023-01-30 20:12:31 +0900

    feat : 3개의 useQuery를 useQuries로 묶어서 사용

--- src/hooks/queries/useGetDuplicateCheck.ts
+++ src/hooks/queries/useGetDuplicateCheck.ts
@@ -1,40 +1,5 @@
 import authApi from '@apis/auth/authApi';
-import { useQueries, useQuery } from 'react-query';
-
-const useGetCheckId = (userId: string) => {
-  return useQuery<any, Error>(
-    'useGetCheckId',
-    () => authApi.getCheckId(userId),
-    {
-      retry: false,
-      refetchOnWindowFocus: false,
-      enabled: !!userId,
-    },
-  );
-};
-
-const useGetCheckEmail = (email: string) => {
-  return useQuery<any, Error>(
-    'useGetCheckEmail',
-    () => authApi.getCheckEmail(email),
-    { retry: false, refetchOnWindowFocus: false, enabled: !!email },
-  );
-};
-
-const useGetCheckNickname = (nickname: string) => {
-  return useQuery<any, Error>(
-    'useGetCheckNickname',
-    () => authApi.getCheckNickname(nickname),
-    { retry: false, refetchOnWindowFocus: false, enabled: !!nickname },
-  );
-};
-
-export {
-  useGetCheckId,
-  useGetCheckEmail,
-  useGetCheckNickname,
-  useGetDuplicateCheck,
-};
+import { useQueries, useQuery, UseQueryOptions } from 'react-query';
 
 interface DuplicateCheck {
   userId: string;
@@ -43,27 +8,30 @@ interface DuplicateCheck {
 }
 
 const useGetDuplicateCheck = ({ userId, email, nickname }: DuplicateCheck) => {
-  return useQueries([
-    {
-      queryKey: 'useGetCheckId',
-      queryFn: () => authApi.getCheckId(userId),
+  return useQueries(
+    [
+      {
+        queryKey: 'useGetCheckId',
+        queryFn: () => authApi.getCheckId(userId),
+        enabled: !!userId,
+      },
+      {
+        queryKey: 'useGetCheckNickname',
+        queryFn: () => authApi.getCheckNickname(nickname),
+        enabled: !!nickname,
+      },
+      {
+        queryKey: 'useGetCheckEmail',
+
+        queryFn: () => authApi.getCheckEmail(email),
+        enabled: !!email,
+      },
+    ].map<UseQueryOptions<any, Error>>((query) => ({
+      ...query,
       retry: false,
       refetchOnWindowFocus: false,
-      enabled: !!userId,
-    },
-    {
-      queryKey: 'useGetCheckEmail',
-      queryFn: () => authApi.getCheckEmail(email),
-      retry: false,
-      refetchOnWindowFocus: false,
-      enabled: !!email,
-    },
-    {
-      queryKey: 'useGetCheckNickname',
-      queryFn: () => authApi.getCheckNickname(nickname),
-      retry: false,
-      refetchOnWindowFocus: false,
-      enabled: !!nickname,
-    },
-  ]);
+    })),
+  );
 };
+
+export { useGetDuplicateCheck };
