9097304fbb4f3c651a519fe4301d1e7e7a33fc92
jooooonis <xowns3213@naver.com>
2023-01-31 11:38:53 +0900

    feat : auction types

---
+++ src/@types/auction.d.ts
@@ -0,0 +1,39 @@
+interface Auction {
+  turn: number;
+  startDate: string;
+  endDate: string;
+  status: string;
+}
+
+interface ArtworkSize {
+  width: number;
+  length: number;
+  height: number;
+  size: number;
+}
+
+interface NowAuctionArtwork {
+  id: number;
+  mainImage: string;
+  title: string;
+  artWorkSize: ArtworkSize;
+  productionYear: number;
+  topPrice: number;
+}
+
+interface LastAutionArtwork extends NowAuctionArtwork {
+  status: string;
+  material: string;
+  bindingCount: number;
+}
+
+interface NowAuctionArtworkList {
+  turn: number;
+  endDate: string;
+  artWorkList: NowAuctionArtwork[];
+}
+
+interface LastAuctionArtworkList {
+  nextPage: boolean;
+  artWorks: LastAutionArtwork[];
+}
