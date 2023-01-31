de67bfaf0c145dafaf0b98adcdaf24e9292f26a9
jooooonis <xowns3213@naver.com>
2023-01-31 14:34:09 +0900

    feat : 경매화면에서 Timer 표시

--- src/pages/auction/index.tsx
+++ src/pages/auction/index.tsx
@@ -1,34 +1,41 @@
 import ArtWorkItem from '@components/auction/ArtWorkItem';
 import AuctionNavigate from '@components/auction/AuctionNavigate';
+import Timer, { TimerProps } from '@components/auction/Timer';
 import Layout from '@components/common/Layout';
+import Loader from '@components/common/Loader';
 import Tab from '@components/common/Tab';
-import Image from 'next/image';
-import React from 'react';
+import useGetNowAuctionArtworkList from '@hooks/queries/auction/useGetNowAuctionArtworkList';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
 import tw from 'tailwind-styled-components';
 
-const ArtworkList = tw.div<DefaultProps>``;
-
-interface DefaultProps {
-  [key: string]: any;
-}
+const ArtworkList = tw.div<defaultProps>``;
 
 export default function Auction() {
+  const { data, isLoading, error } = useGetNowAuctionArtworkList();
+  const [time, setTime] = useState<TimerProps>();
+
+  useEffect(() => {
+    if (!data) return;
+    const now = moment();
+    const endDate = moment(data.endDate, 'YYYY-MM-DD HH:mm:ss');
+    const diff = endDate.diff(now, 'seconds');
+    const day = Math.floor(diff / (60 * 60 * 24));
+    const hour = Math.floor((diff % (60 * 60 * 24)) / (60 * 60));
+    const minute = Math.floor((diff % (60 * 60)) / 60);
+    const second = Math.floor(diff % 60);
+    setTime({ day, hour, minute, second });
+  }, [data]);
+
+  if (isLoading) return <Loader />;
+
   return (
     <>
       <Layout>
         <AuctionNavigate />
         <section className="relative mb-7 flex justify-between">
-          <span className="text-[20px] font-bold">제 1회 아띠즈 경매</span>
-          <div className=" flex items-center rounded border border-brand px-2">
-            <Image
-              alt=""
-              src="/svg/icons/icon_clock_brand.svg"
-              width="14"
-              height="0"
-              className="mr-1"
-            />
-            <span className="text-[14px] font-bold">08:20:03</span>
-          </div>
+          <span className="text-[20px] font-bold">{`제 ${data?.turn}회 아띠즈 경매`}</span>
+          {time && <Timer {...time} />}
         </section>
         <ArtworkList>
           <ArtWorkItem />
