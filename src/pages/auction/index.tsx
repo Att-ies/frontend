3c87e2b30081b281217d14c42dcfb0e55837c286
jooooonis <xowns3213@naver.com>
2023-01-31 14:40:34 +0900

    chore : 변수 이름 변경

--- src/pages/auction/index.tsx
+++ src/pages/auction/index.tsx
@@ -5,6 +5,7 @@ import Layout from '@components/common/Layout';
 import Loader from '@components/common/Loader';
 import Tab from '@components/common/Tab';
 import useGetNowAuctionArtworkList from '@hooks/queries/auction/useGetNowAuctionArtworkList';
+import { secondToDate } from '@utils/secondToDate';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 import tw from 'tailwind-styled-components';
@@ -13,18 +14,14 @@ const ArtworkList = tw.div<defaultProps>``;
 
 export default function Auction() {
   const { data, isLoading, error } = useGetNowAuctionArtworkList();
-  const [time, setTime] = useState<TimerProps>();
+  const [date, setDate] = useState<TimerProps>();
 
   useEffect(() => {
     if (!data) return;
     const now = moment();
     const endDate = moment(data.endDate, 'YYYY-MM-DD HH:mm:ss');
     const diff = endDate.diff(now, 'seconds');
-    const day = Math.floor(diff / (60 * 60 * 24));
-    const hour = Math.floor((diff % (60 * 60 * 24)) / (60 * 60));
-    const minute = Math.floor((diff % (60 * 60)) / 60);
-    const second = Math.floor(diff % 60);
-    setTime({ day, hour, minute, second });
+    setDate(secondToDate(diff));
   }, [data]);
 
   if (isLoading) return <Loader />;
@@ -35,7 +32,7 @@ export default function Auction() {
         <AuctionNavigate />
         <section className="relative mb-7 flex justify-between">
           <span className="text-[20px] font-bold">{`제 ${data?.turn}회 아띠즈 경매`}</span>
-          {time && <Timer {...time} />}
+          {date && <Timer {...date} />}
         </section>
         <ArtworkList>
           <ArtWorkItem />
