ce468b72832b4f5ddd77dbec77835d3450f32324
jooooonis <xowns3213@naver.com>
2023-02-01 11:43:42 +0900

    feat : 모바일, 웹 레아이웃

--- src/pages/auction/[id].tsx
+++ src/pages/auction/[id].tsx
@@ -10,25 +10,22 @@ const LayoutBox = styled.div`
 export default function Detail() {
   return (
     <LayoutBox className="relative h-full w-full max-w-[420px] overflow-y-scroll bg-white">
-      <div
-        className="absolute inset-x-0
-         h-[306px] w-full bg-slate-500"
-      >
-        <div className="flex h-16 w-full items-center justify-between px-6">
-          <Image
-            alt="clock"
-            src="/svg/icons/auction/icon_arrow_white.svg"
-            width="24"
-            height="24"
-          />
-          <Image
-            alt="clock"
-            src="/svg/icons/auction/icon_heart_white.svg"
-            width="24"
-            height="24"
-          />
-        </div>
+      <div className="fixed inset-x-0 top-0 mx-auto flex h-16 w-full max-w-[420px] items-center justify-between px-6">
+        <Image
+          alt="clock"
+          src="/svg/icons/auction/icon_arrow_white.svg"
+          width="24"
+          height="24"
+        />
+        <Image
+          alt="clock"
+          src="/svg/icons/auction/icon_heart_white.svg"
+          width="24"
+          height="24"
+        />
       </div>
+      <div className="h-[306px] w-full bg-slate-500"></div>
+      <div className="-mt-4 h-[2000px] w-full rounded-t-2xl bg-blue-500"></div>
     </LayoutBox>
   );
 }
