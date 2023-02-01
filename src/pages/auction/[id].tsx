49625107abbe6fa1262dcfce69f75f891da938f4
jooooonis <xowns3213@naver.com>
2023-02-01 14:11:39 +0900

    feat : IntersectionObserver를 사용하여 스크롤 감지

--- src/pages/auction/[id].tsx
+++ src/pages/auction/[id].tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 const LayoutBox = styled.div`
@@ -8,8 +9,28 @@ const LayoutBox = styled.div`
 `;
 
 export default function Detail() {
+  const target = useRef<HTMLDivElement | null>(null);
+  const onIntersect = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log('intersecting!!!');
+      }
+    });
+  };
+
+  useEffect(() => {
+    let observer;
+    if (target) {
+      observer = new IntersectionObserver(onIntersect, {
+        rootMargin: `0px 0px -${window.innerHeight - 64}px 0px`,
+      });
+      observer.observe(target.current);
+    }
+    return () => observer && observer.disconnect();
+  }, [target]);
+
   return (
-    <LayoutBox className="relative h-full w-full max-w-[420px] overflow-y-scroll bg-white">
+    <LayoutBox className="relative h-full w-full max-w-[420px] overflow-y-scroll scroll-smooth border border-red-500 bg-white">
       <div className="fixed inset-x-0 top-0 mx-auto flex h-16 w-full max-w-[420px] items-center justify-between px-6">
         <Image
           alt="clock"
@@ -25,7 +46,10 @@ export default function Detail() {
         />
       </div>
       <div className="h-[306px] w-full bg-slate-500"></div>
-      <div className="-mt-4 h-[2000px] w-full rounded-t-2xl bg-blue-500"></div>
+      <div
+        ref={target}
+        className="-mt-4 h-[2000px] w-full rounded-t-2xl bg-blue-500"
+      ></div>
     </LayoutBox>
   );
 }
