024988a38fe72aa1f081e92bb294d067212131b1
jooooonis <xowns3213@naver.com>
2023-02-02 22:10:09 +0900

    feat : query에 type 추가

--- src/hooks/queries/useGetDetail.ts
+++ src/hooks/queries/useGetDetail.ts
@@ -2,7 +2,7 @@ import artworkApi from '@apis/artwork/artworkApi';
 import { useQuery } from 'react-query';
 
 const useGetDetail = (artWorkId: number) => {
-  return useQuery<any, Error>(
+  return useQuery<ArtworkDetail, Error>(
     'useGetDetail',
     () => artworkApi.getDetail(artWorkId),
     { retry: false, refetchOnWindowFocus: false },
