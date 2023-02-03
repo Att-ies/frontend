5e4f9235584e69130db7928e82e9c85e3cba26c9
GeonwooShin <rjsdnslawkd@naver.com>
2023-02-03 11:54:23 +0900

    feat : 남은 일수 계산 함수 생성

--- src/components/exhibition/AuctionItem.tsx
+++ src/components/exhibition/AuctionItem.tsx
@@ -1,20 +1,21 @@
+import moment from 'moment';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
-interface AuctionItemForm {
-  src: string;
-  date: string;
-  artRegister: number;
-  status: boolean;
-  id: number;
-}
-
 interface AuctionListProps {
-  auctionList: AuctionItemForm;
+  auctionList: ExhibitListProps;
 }
 
 export default function AuctionItem({ auctionList }: AuctionListProps) {
   const router = useRouter();
+
+  const remainedDay = () => {
+    const currentDate = moment().unix() * 1000;
+    const endDate = moment(auctionList.endDate, 'YYYY-MM-DD').unix() * 1000;
+    const diff = moment.duration(endDate - currentDate);
+    return diff.days();
+  };
+
   return (
     <div
       className="mb-5 flex cursor-pointer border-b-[1px] pb-6 last:mb-0"
@@ -26,21 +27,20 @@ export default function AuctionItem({ auctionList }: AuctionListProps) {
       }
     >
       <div className="relative mr-2 h-[99px] w-[82px] rounded">
-        <Image
-          src={auctionList.src}
-          alt="notification"
-          fill
-          style={{
-            objectFit: 'cover',
-          }}
-          className="rounded"
-          quality={100}
-        />
+        {auctionList.image && (
+          <Image
+            src={auctionList.image}
+            alt="notification"
+            fill
+            className="rounded object-cover"
+            quality={100}
+          />
+        )}
       </div>
       <div className="flex flex-col justify-center">
-        {auctionList.status ? (
+        {auctionList.status === 'processing' ? (
           <span className="h-[17px] w-[42px] bg-[#FC6554] text-center text-10 text-[#FFFFFF]">
-            D-7
+            D-{remainedDay()}
           </span>
         ) : (
           <span className="h-[17px] w-[42px] bg-[#191919] text-center text-10 text-[#FFFFFF]">
@@ -48,12 +48,14 @@ export default function AuctionItem({ auctionList }: AuctionListProps) {
           </span>
         )}
         <span className="text-16 font-semibold text-[#191919]">
-          제 {auctionList.id}회 아띠즈 경매
+          제 {auctionList.turn}회 아띠즈 경매
         </span>
         <span className="text-14 text-[#767676]">
-          작품 수 : {auctionList.artRegister}
+          작품 수 : {auctionList.artWorkCount}
         </span>
-        <span className="text-11 text-[#767676]">{auctionList.date}</span>
+        <span className="text-11 text-[#767676]">
+          {auctionList.startDate}~{auctionList.endDate}
+        </span>
       </div>
     </div>
   );
