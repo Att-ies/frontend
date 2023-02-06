52b37cbf648cde98980f173e3a51136fa34c8655
jooooonis <xowns3213@naver.com>
2023-02-06 12:00:25 +0900

    feat : 나의 경매 아이템 컴포넌트 디자인 수정

--- src/components/profile/SuccessBidArtItem.tsx
+++ src/components/profile/SuccessBidArtItem.tsx
@@ -1,57 +1,46 @@
-import { useRouter } from 'next/router';
 import Image from 'next/image';
 import React from 'react';
 import tw from 'tailwind-styled-components';
 
-interface ArtItemProps {
-  biddingItem: BidArtworkForm;
+interface SuccessBidArtItemProps {
+  biddingItem: SuccessfulBidArtworkForm;
   [key: string]: any;
   handleOption?: () => void;
 }
 
-const ArtItemTag = tw.section<ArtItemProps>`
+const SuccessBidArtItemTag = tw.section<SuccessBidArtItemProps>`
 flex mt-5 border-b last:border-none border-[#EDEDED] pb-5 relative
 `;
 
-export default function BidArtItem({
+export default function SuccessBidArtItem({
   biddingItem,
   handleOption,
   ...rest
-}: ArtItemProps) {
-  const router = useRouter();
+}: SuccessBidArtItemProps) {
   return (
-    <ArtItemTag
-      {...rest}
-      onClick={() => {
-        router.push(`/auction/${biddingItem?.id}`);
-      }}
-    >
+    <SuccessBidArtItemTag {...rest}>
       <article className="relative h-[100px] w-[82px] overflow-hidden rounded">
         <Image
           alt="example"
           src={biddingItem?.mainImage || '/svg/example/example_picture_col.svg'}
           fill
-          className="object-fill"
+          className="object-cover"
         />
       </article>
       <article className="ml-3 pt-1">
         <p className="text-12 font-semibold text-[#767676]">
           제 {biddingItem?.turn}회 아띠즈 경매
         </p>
-        <p>
+        <p className="mt-1">
           <span className="text-14">{biddingItem?.title}</span>
           <span className="text-12"> | {biddingItem?.artistName}</span>
         </p>
-        <div className="mt-2">
-          <p className="text-12 font-semibold text-[#767676]">
-            나의 입찰가 {biddingItem?.myBiddingPrice}원
-          </p>
-          <p className="font-semibold">
-            <span className="text-12">최종 입찰가</span>
-            <span className="text-14"> {biddingItem?.finalBiddingPrice}원</span>
+        <div className="mt-[1.5px]">
+          <p className="text-16 font-semibold text-brand">
+            {biddingItem?.finalBiddingPrice}원
           </p>
         </div>
       </article>
-    </ArtItemTag>
+    </SuccessBidArtItemTag>
   );
 }
