0b624b1068cd227b4f13d359a1ca3e806a5c418d
GeonwooShin <rjsdnslawkd@naver.com>
2023-02-08 13:35:24 +0900

    feat : 문의 수정 mutation 생성

---
+++ src/hooks/mutations/usePatchInquiry.ts
@@ -0,0 +1,32 @@
+import profileApi from '@apis/profile/profileApi';
+import { queryClient } from 'pages/_app';
+import { useMutation } from 'react-query';
+
+const usePatchInquiry = (inquiryId: number, formData: any) => {
+  return useMutation<any, Error>(
+    'usePatchInquiry',
+    () => profileApi.patchInquiry(inquiryId, formData),
+    {
+      onMutate: async () => {
+        await queryClient.cancelQueries({ queryKey: ['usePatchInquiry'] });
+        const previousValue = queryClient.getQueryData(['useGetInquiry']);
+        queryClient.setQueryData(['useGetInquiry'], (old: any) => {
+          return {
+            ...old,
+            formData,
+          };
+        });
+        return { previousValue };
+      },
+      onError: (context: any) => {
+        queryClient.setQueryData(['useGetInquiry'], context.previousValue);
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries({
+          queryKey: ['useGetInquiry'],
+        });
+      },
+    },
+  );
+};
+export default usePatchInquiry;
