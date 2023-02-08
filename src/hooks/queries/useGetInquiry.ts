bb52e2c363d435936cfad7f7397cd7a8ecc656fb
GeonwooShin <rjsdnslawkd@naver.com>
2023-02-08 12:59:16 +0900

    fix : 타입 변경

--- src/hooks/queries/useGetInquiry.ts
+++ src/hooks/queries/useGetInquiry.ts
@@ -1,16 +1,13 @@
 import { useQuery } from 'react-query';
 import profileApi from '@apis/profile/profileApi';
 
-interface Inquiry {
-  date: string;
-  time: string;
-  title: string;
-  content: string;
-  status: string;
-  answer: string;
-  id: number;
-}
-
 export default function useGetInquiry() {
-  return useQuery<any, Error>('useInquiry', () => profileApi.getInquiry());
+  return useQuery<Inquiry[], Error>(
+    'useGetInquiry',
+    () => profileApi.getInquiry(),
+    {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  );
 }
