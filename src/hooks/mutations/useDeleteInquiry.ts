860d21979ce5e3f19c58fd799da002935ccdc336
GeonwooShin <rjsdnslawkd@naver.com>
2023-02-08 13:00:25 +0900

    feat : 문의 삭제 낙관적 업데이트

---
+++ src/hooks/mutations/useDeleteInquiry.ts
@@ -0,0 +1,30 @@
+import profileApi from '@apis/profile/profileApi';
+import { queryClient } from 'pages/_app';
+import { useMutation } from 'react-query';
+
+const useDeleteInquiry = (inquiryId: number) => {
+  return useMutation<any, Error>(
+    'useDeleteInquiry',
+    () => profileApi.deleteInquiry(inquiryId),
+    {
+      onMutate: async () => {
+        await queryClient.cancelQueries({ queryKey: ['useDeleteInquiry'] });
+        const previousValue = queryClient.getQueryData(['useGetInquiry']);
+        queryClient.setQueryData(['useGetInquiry'], (old: any) =>
+          old.filter((t) => t.id !== inquiryId),
+        );
+        return { previousValue };
+      },
+      onError: (context: any) => {
+        queryClient.setQueryData(['useGetInquiry'], context.previousValue);
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries({
+          queryKey: ['useGetInquiry'],
+        });
+      },
+    },
+  );
+};
+
+export default useDeleteInquiry;
