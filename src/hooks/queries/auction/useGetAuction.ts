2501dab5727476dee3828bde29eb836d74ed903b
Kuesung Park <gueit214@naver.com>
2023-02-11 11:25:30 +0900

    refactor : 타입 추가

--- src/hooks/queries/auction/useGetAuction.ts
+++ src/hooks/queries/auction/useGetAuction.ts
@@ -3,19 +3,23 @@ import homeApi from '@apis/home/homeApi';
 import moment from 'moment';
 
 const useGetAuction = () => {
-  return useQuery('useGetAuction', () => homeApi.getAuctionList(), {
-    retry: false,
-    refetchOnWindowFocus: false,
-    select: (data) => {
-      return data.map((it: AuctionList) => {
-        return {
-          ...it,
-          startDate: moment(it.startDate, 'YYYY-MM-DD-hh-mm-ss'),
-          endDate: moment(it.endDate, 'YYYY-MM-DD-hh-mm-ss'),
-        };
-      });
+  return useQuery<AuctionList[], Error>(
+    'useGetAuction',
+    () => homeApi.getAuctionList(),
+    {
+      retry: false,
+      refetchOnWindowFocus: false,
+      select: (data) => {
+        return data.map((it: AuctionList) => {
+          return {
+            ...it,
+            startDate: moment(it.startDate, 'YYYY-MM-DD-hh-mm-ss'),
+            endDate: moment(it.endDate, 'YYYY-MM-DD-hh-mm-ss'),
+          };
+        });
+      },
     },
-  });
+  );
 };
 
 export default useGetAuction;
