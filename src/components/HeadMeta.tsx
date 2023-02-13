dfeb66157be833cb31e67e2a276af9f36205768b
Kuesung Park <gueit214@naver.com>
2023-02-13 13:24:39 +0900

    refactor : 수정

--- src/components/HeadMeta.tsx
+++ src/components/HeadMeta.tsx
@@ -9,9 +9,9 @@ const HeadMeta = () => {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta property="og:title" content="아띠즈:예술:" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="attiess.netlify.app/" />
+      <meta property="og:url" content="https://attiess.netlify.app/" />
       <meta property="og:image" content="/public/svg/icons/icon_logo.svg" />
-      <meta property="og:article:author" content="정리습관" />
+      <meta property="og:article:author" content="졸업작품 거래 플랫폼" />
     </Head>
   );
 };
