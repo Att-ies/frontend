0845c5fdb432749c418f2cc24825d2806a93a82f
Kuesung Park <gueit214@naver.com>
2023-02-13 13:34:09 +0900

    feat : 모바일 화면에서 확대 안되도록

--- src/components/HeadMeta.tsx
+++ src/components/HeadMeta.tsx
@@ -6,7 +6,10 @@ const HeadMeta = () => {
       <link rel="shortcut icon" href="/static/favicon.ico" />
       <title>Atties</title>
       <meta name="description" content="졸업작품 거래 플랫폼, 아띠즈:예술:" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no"
+      />
       <meta property="og:title" content="아띠즈:예술:" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://attiess.netlify.app/" />
