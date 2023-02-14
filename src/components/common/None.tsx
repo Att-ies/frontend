845e9834233b374de93e7df41113e56b022bf776
jooooonis <xowns3213@naver.com>
2023-02-15 01:05:48 +0900

    feat : absolute로 자리 조정 및 탭 버튼 안 눌리는 현상 해결

--- src/components/common/None.tsx
+++ src/components/common/None.tsx
@@ -8,7 +8,7 @@ interface NoneProps {
 
 export default function None({ path, message }: NoneProps) {
   return (
-    <div className="flex h-[calc(100%-80px)] flex-col items-center justify-center">
+    <div className="pointer-events-none absolute inset-0 z-0 flex flex-col items-center justify-center">
       <Image src={`/svg/none/${path}.svg`} width={40} height={40} alt="wish" />
       <p className="mt-2 text-16 font-medium text-[#999999]">{message}</p>
     </div>
