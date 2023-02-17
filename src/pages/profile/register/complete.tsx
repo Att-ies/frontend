f3a676731552375c1c7d8ea46404b31c5c6ae230
Kuesung Park <gueit214@naver.com>
2023-02-17 13:23:20 +0900

    feat : instagram 링크 추가

--- src/pages/profile/register/complete.tsx
+++ src/pages/profile/register/complete.tsx
@@ -3,6 +3,7 @@ import Navigate from '@components/common/Navigate';
 import React from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Complete() {
   const router = useRouter();
@@ -26,6 +27,10 @@ export default function Complete() {
         <div className="#767676 text-12 text-[#767676]">
           <p>아띠즈에서 학력 인증 확인 후 알림을 드리겠습니다.</p>
           <p>심사는 3일(영업일) 이내 진행됩니다.</p>
+        </div>
+        <div className=" z-50 text-12">
+          <p>instagram으로 연락 주시면 빠르 시일 내에 확인하겠습니다.</p>
+          <Link href="https://www.instagram.com/atties_art/">@atties_art</Link>
         </div>
       </section>
     </Layout>
