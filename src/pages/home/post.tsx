99e71cdcbe7dd97c4738e24c1d18601c4925922d
jooooonis <xowns3213@naver.com>
2023-02-18 03:33:15 +0900

    refactor : 컴포넌트 사용

--- src/pages/home/post.tsx
+++ src/pages/home/post.tsx
@@ -19,6 +19,7 @@ import { makeBlob } from '@utils/makeBlob';
 import useGetProfile from '@hooks/queries/useGetProfile';
 import { today } from '@utils/today';
 import { dataURLtoFile } from '@utils/dataURLtoFile';
+import Guarantee from '@components/auction/Guarantee';
 
 const ARTWORK_STATUS = [
   { value: '매우 좋음' },
@@ -483,75 +484,17 @@ export default function Post() {
           watch('height') &&
           watch('genre') &&
           signature && (
-            <div
-              ref={guaranteeRef}
-              className="m-auto min-w-[327px] flex-col items-center justify-center py-9"
-            >
-              <div className="text-center text-16 font-semibold tracking-[0.3em]">
-                작 품 보 증 서
-              </div>
-              <p className="text-center text-[8px] font-light tracking-[-0.05em] text-[#A5A5A5]">
-                CERTIFICATE OF AUTHENTICITY
-              </p>
-              <div className="relative mx-auto mt-7 h-[74px] w-[116px]">
-                <Image
-                  src={makeBlob(fileList[0])}
-                  fill
-                  className="object-cover"
-                  alt="artwork"
-                />
-              </div>
-              <div className="mt-3 flex justify-center text-11 leading-5">
-                <div className="flex-col font-semibold">
-                  <p>작가</p>
-                  <p>제목</p>
-                  <p>제작년도</p>
-                  <p>작품크기</p>
-                  <p>제작기법</p>
-                </div>
-                <div className="ml-12 flex-col">
-                  <p>{user?.nickname}</p>
-                  <p>{watch('title')}</p>
-                  <p>{watch('productionYear')}</p>
-                  <p>{`${watch('width')}x${watch('length')}x${watch(
-                    'height',
-                  )}cm`}</p>
-                  <p>{watch('genre')}</p>
-                </div>
-              </div>
-              <div className="mt-4 flex-col">
-                <div className="flex items-center justify-center">
-                  <Image src={signature} width={50} height={50} alt="artwork" />
-                </div>
-                <div className="mt-2 flex-col items-center justify-center">
-                  <div className="mx-auto w-[70px] border-t border-t-black pb-[3px]" />
-                  <div className="text-center text-[8px]  text-[#A5A5A5]">
-                    Artist Signature
-                  </div>
-                </div>
-              </div>
-              <ul className="mt-3 w-full list-none text-center text-[8px]">
-                <li className="w-full  text-black  before:mr-2  before:content-['\2022']">
-                  본 작품은 위에 서명한 작가의 작품임을 보증합니다.
-                </li>
-                <li className="w-full text-black  before:mr-2  before:content-['\2022']">
-                  본 작품은 일체의 모작, 위작이 아님을 보증합니다.
-                </li>
-                <li className="w-full text-black  before:mr-2  before:content-['\2022']">
-                  본 보증서는 작품 보증 이외 환불, 교환 등의 목적으로 사용이
-                  불가합니다.
-                </li>
-              </ul>
-              <p className="my-3 text-center text-[8px]">{today()}</p>
-              <div className="flex items-center justify-center text-brand">
-                <Image
-                  src="/svg/post/logo_small.svg"
-                  width={60}
-                  height={10}
-                  alt="logo"
-                />
-              </div>
-            </div>
+            <Guarantee
+              nickname={user.nickname}
+              title={watch('title')}
+              productionYear={watch('productionYear')}
+              width={watch('width')}
+              length={watch('length')}
+              height={watch('height')}
+              genre={watch('genre')}
+              guaranteeImage={signature}
+              mainImage={makeBlob(fileList[0])}
+            />
           )}
 
         <div className="relative h-[336px]">
