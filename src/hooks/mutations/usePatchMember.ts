1c34d866bd6f2ee51bf3a33a1016133eeae8d1bb
Kuesung Park <gueit214@naver.com>
2023-02-17 13:12:21 +0900

    feat : 라우팅 수정

--- src/hooks/mutations/usePatchMember.ts
+++ src/hooks/mutations/usePatchMember.ts
@@ -4,27 +4,15 @@ import { useMutation } from 'react-query';
 
 const usePatchUser = () => {
   const router = useRouter();
-  return useMutation<any, Error, FormData>(
-    'useUserMuation',
-    (data) => profileApi.patchUser(data),
-    {
-      onSuccess: () => {
-        router.push('/home');
-      },
-    },
+  return useMutation<any, Error, FormData>('useUserMuation', (data) =>
+    profileApi.patchUser(data),
   );
 };
 
 const usePatchArtist = () => {
   const router = useRouter();
-  return useMutation<any, Error, FormData>(
-    'useArtistMuation',
-    (data) => profileApi.patchArtist(data),
-    {
-      onSuccess: () => {
-        router.push('/home');
-      },
-    },
+  return useMutation<any, Error, FormData>('useArtistMuation', (data) =>
+    profileApi.patchArtist(data),
   );
 };
 
