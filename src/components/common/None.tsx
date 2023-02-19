e7def9b683d1cc54b9ee54f10ac9947df95f5ce0
Kuesung Park <gueit214@naver.com>
2023-02-19 19:25:04 +0900

    fix : 오타 수정

--- src/components/common/None.tsx
+++ src/components/common/None.tsx
@@ -9,7 +9,7 @@ interface NoneProps {
 export default function None({ path, message }: NoneProps) {
   return (
     <div className="pointer-events-none absolute inset-0 z-0 flex flex-col items-center justify-center">
-      <Image src={`/svg/none/${path}.svg`} width={40} height={40} alt="wish" />
+      <Image src={`/svg/none/${path}.svg`} width={40} height={40} alt="none" />
       <p className="mt-2 text-16 font-medium text-[#999999]">{message}</p>
     </div>
   );
