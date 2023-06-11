345bc412f92d2eab3cca193edca591656ef9ea8a
Kuesung Park <gueit214@naver.com>
2023-06-11 11:50:03 +0900

    refactor : Layout 컴포넌트 제거

--- src/components/common/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface LayoutProps {
-  children?: React.ReactNode;
-}
-
-export default function Layout({ children }: LayoutProps) {
-  return <div className="">{children}</div>;
-}
