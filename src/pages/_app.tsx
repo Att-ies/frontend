aa9e1499e2e015af546547be681f54f84c65d28e
Kuesung Park <gueit214@naver.com>
2023-06-11 12:05:24 +0900

    refactor : 타입 추가, 코드 순서 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,36 @@
 import '../styles/globals.css';
-
-import Loader from '@components/common/Loader';
-import GoogleScript from '@components/GoogleScript';
-import store from '@features/store';
-import { useWindowSize } from '@hooks/common/useWindowSize';
+import { useRouter } from 'next/router';
+import { useEffect, useState, Suspense } from 'react';
+import { Provider } from 'react-redux';
 import {
-  Hydrate,
   QueryClient,
   QueryClientProvider,
+  Hydrate,
 } from '@tanstack/react-query';
-import { pageview } from '@utils/gtag';
-import axios from 'axios';
+import { PersistGate } from 'redux-persist/integration/react';
+import styled from 'styled-components';
+import { useWindowSize } from '@hooks/common/useWindowSize';
 import { getCookie } from 'cookies-next';
 import type { AppContext, AppProps } from 'next/app';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { PropsWithChildren, Suspense, useEffect, useState } from 'react';
-import { Provider } from 'react-redux';
+import axios from 'axios';
+import Loader from '@components/common/Loader';
+import GoogleScript from '@components/GoogleScript';
+import store from '@features/store';
 import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
+import { pageview } from '@utils/gtag';
 
-import styled from 'styled-components';
-
-const persistor = persistStore(store);
-interface AppExtendedProps extends AppProps {
-  userData: User;
-}
-let vh = 0;
+const LayoutCss = styled.div`
+  min-height: calc(var(--var, 1vh) * 200);
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  position: relative;
+  max-width: 26.25rem;
+  overflow-y: scroll;
+  background-color: white;
+  padding: 1.5rem;
+`;
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -38,43 +42,58 @@ const queryClient = new QueryClient({
     mutations: { retry: false, useErrorBoundary: true },
   },
 });
+
+const persistor = persistStore(store);
+
+interface AppExtendedProps extends AppProps {
+  userData: User;
+}
+
 export default function App({
   Component,
   pageProps,
   userData,
 }: AppExtendedProps) {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
-  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    const start = () => {
-      setLoading(true);
-    };
-    const end = () => {
-      setLoading(false);
-    };
     const handleRouteChange = (url) => {
+      setLoading(true);
       pageview(url);
     };
 
-    router.events.on('routeChangeStart', start);
-    router.events.on('routeChangeComplete', end);
+    const handleRouteChangeEnd = () => setLoading(false);
+
+    router.events.on('routeChangeStart', handleRouteChange);
     router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteChangeEnd);
     router.events.on('hashChangeComplete', handleRouteChange);
-    router.events.on('routeChangeError', end);
+    router.events.on('routeChangeError', handleRouteChangeEnd);
 
     return () => {
-      router.events.off('routeChangeStart', start);
-      router.events.off('routeChangeComplete', end);
+      router.events.off('routeChangeStart', handleRouteChange);
       router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteChangeEnd);
       router.events.off('hashChangeComplete', handleRouteChange);
-      router.events.off('routeChangeError', end);
+      router.events.off('routeChangeError', handleRouteChangeEnd);
     };
-  }, []);
+  }, [router.events]);
 
-  return loading ? (
-    <Loader />
-  ) : (
+  const Layout = ({ children }) => {
+    const windowSize = useWindowSize();
+
+    useEffect(() => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }, [windowSize.height]);
+
+    return <LayoutCss>{children}</LayoutCss>;
+  };
+
+  if (loading) return <Loader />;
+
+  return (
     <div className="flex h-screen w-screen justify-center bg-slate-50 ">
       <Head>
         <title>Atties</title>
@@ -97,40 +116,17 @@ export default function App({
   );
 }
 
-export { queryClient };
-
-function Layout({ children }: PropsWithChildren<{}>) {
-  const windowSize = useWindowSize();
-
-  useEffect(() => {
-    vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }, [windowSize.height]);
-
-  return <LayoutCss>{children}</LayoutCss>;
-}
-
-const LayoutCss = styled.div`
-  min-height: calc(var(--var, 1vh) * 200);
-  width: 100%;
-  height: 100%;
-  margin: 0 auto;
-  position: relative;
-  max-width: 26.25rem;
-  overflow-y: scroll;
-  background-color: white;
-  padding: 1.5rem;
-`;
-
 App.getInitialProps = async ({ Component, ctx }: AppContext) => {
   let pageProps = {};
+
   if (Component.getInitialProps) {
     pageProps = await Component.getInitialProps(ctx);
   }
+
   const accessToken = getCookie('accessToken', ctx);
 
   try {
-    let res = await axios(
+    const res = await axios(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/members/me`,
       {
         headers: {
@@ -138,9 +134,10 @@ App.getInitialProps = async ({ Component, ctx }: AppContext) => {
         },
       },
     );
+
     return { pageProps, userData: res.data };
   } catch (err) {
-    console.log(err);
+    console.error('Failed to get user data:', err);
     return { pageProps };
   }
 };
